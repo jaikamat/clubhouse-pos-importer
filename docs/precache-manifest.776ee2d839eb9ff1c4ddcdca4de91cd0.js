90dc4a0835c03f8a9393daa611ad4d0723b5db28
Admin <jaipkamat@gmail.com>
2019-11-06 14:03:59 -0800

    Test deploy

---
+++ docs/precache-manifest.776ee2d839eb9ff1c4ddcdca4de91cd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116d234d4f8e98bb87a3fde71a0e75c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db6331543c8e4beab5e3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2cf8e98ff9591f6f684f",
+    "url": "/static/js/2.0cbcd13a.chunk.js"
+  },
+  {
+    "revision": "db6331543c8e4beab5e3",
+    "url": "/static/js/main.b9e63380.chunk.js"
+  },
+  {
+    "revision": "e872bc308019a0ab5662",
+    "url": "/static/js/runtime-main.8aab2d76.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);

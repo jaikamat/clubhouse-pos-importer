a30bcf5e2359fbe56b1bcc348508e89d1e8916f4
Admin <jaipkamat@gmail.com>
2019-11-08 12:30:29 -0800

    Split Main.js component into react router components

---
+++ src/AuthCallback.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AuthCallback() {
+    return <p>THIS IS ANOTHER PAGE, YO</p>;
+}
+
+export default AuthCallback;
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Header() {
-    return <p>HELLO THERE</p>;
+    return <p>THIS IS THE NAVBAR</p>;
 }
 
 export default Header;

3b80fe9dd8fc946df0e2f7fdeb2b9dee646e6b75
Admin <jaipkamat@gmail.com>
2019-11-08 18:20:58 -0800

    Added query-string dependency, parsing queryParams in AuthCallback route

--- src/AuthCallback.js
+++ src/AuthCallback.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import queryString from 'query-string';
 
-function AuthCallback() {
-    return <p>THIS IS ANOTHER PAGE, YO</p>;
+// This will receive the callback from the OAuth login
+// Depending on the queryparams, we should:
+// 1. Fetch the actual token from LightSpeed
+// 2. Commit them to localStorage
+// Then redirect back to the homepage
+
+class AuthCallback extends React.Component {
+    render() {
+        console.log(this.props);
+        return (
+            <p>
+                Testing callback. Search params:{' '}
+                {JSON.stringify(queryString.parse(this.props.location.search))}
+            </p>
+        );
+    }
 }
 
 export default AuthCallback;

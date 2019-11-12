7fc775e9238872512ac17f789a739628173ed5f2
Admin <jaipkamat@gmail.com>
2019-11-12 11:19:11 -0800

    Selecting autocomplete fetches cards by title

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 class Home extends React.Component {
-    state = { searchTerm: '' };
+    state = { searchResults: [] };
 
-    handleSearchSelect = term => {
-        this.setState({ searchTerm: term });
+    handleSearchSelect = async term => {
+        try {
+            const { data } = await axios.get(
+                encodeURI(`https://api.scryfall.com/cards/search?q=!"${term}"`)
+            );
+            console.log(data.data);
+            // this.setState({ searchResults: data.data });
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     render() {
@@ -16,9 +25,9 @@ class Home extends React.Component {
                         Creating an app for my Local Game Shop (LGS) to manage their custom
                         inventory via LightSpeed POS!
                     </p>
-                    <p>{this.state.searchTerm}</p>
 
                     <SearchBar handleSearchSelect={this.handleSearchSelect} />
+                    <p>{this.state.searchResults}</p>
                 </header>
             </div>
         );

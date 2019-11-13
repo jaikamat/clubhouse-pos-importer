a8c6286393b49b8d709b93232f1ddcb57b396665
Admin <jaipkamat@gmail.com>
2019-11-13 14:45:05 -0800

    Added dotenv dependency and WIP on google add item function

--- gc_functions/addCardToInventory.js
+++ gc_functions/addCardToInventory.js
@@ -1,3 +1,44 @@
-exports.addCardToInventory = function(req, res) {
-    res.send('Card Added!');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+require('dotenv').config();
+
+async function addCardToInventory(data) {
+    const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0-uytsf.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+    const client = await new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    try {
+        await client.connect();
+        console.log('Successfully connected to mongo');
+
+        const db = client.db('test');
+
+        let d = await db
+            .collection('card_inventory')
+            .insertOne({ hello: 'world' });
+
+        // get item
+        // if no item, create item
+        // _id = scryfallId and
+
+        assert.equal(1, d.insertedCount);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await client.close();
+    }
+}
+
+exports.writeCardDataToDB = async (req, res) => {
+    try {
+        await writeData(req.body);
+        res.status(200).send('DB was updated');
+    } catch (e) {
+        console.log(e);
+        res.status(500).send('Server error');
+    }
 };
+
+exports.addCardToInventory = addCardToInventory;

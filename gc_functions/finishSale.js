e69b8e129d1839338e8c4e0b2b1fc56e31a8c15b
Admin <jaipkamat@gmail.com>
2019-12-18 17:41:57 -0800

    FinalizeSale function stores saleData to mongo

--- gc_functions/finishSale.js
+++ gc_functions/finishSale.js
@@ -3,12 +3,20 @@ const axios = require('axios');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
+/**
+ * Helper fn used to create employee-readable note lines in the Lightspeed POS system
+ * @param {Object} card - the card involved in the transaction
+ */
 function createSaleLine(card) {
     const { price, qtyToSell, finishCondition, name, set_name } = card;
 
     return `${name} | ${set_name} | Qty sold: ${qtyToSell} | Condition: ${finishCondition} | Price/unit: ${price}`;
 }
 
+/**
+ * Updates the Mongo inventory based on the card's passed properties (qtyToSell, finishCondition, id, name)
+ * @param {Object} card - the card involved in the transaction
+ */
 async function updateCardInventory(card) {
     const { qtyToSell, finishCondition, id, name } = card;
     const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0-uytsf.gcp.mongodb.net/test?retryWrites=true&w=majority`;
@@ -82,6 +90,12 @@ async function updateCardInventory(card) {
     }
 }
 
+/**
+ * Creates a sale via the Lightspeed API
+ *
+ * @param {String} authToken - the store's OAuth access token
+ * @param {Array} cards - array of cards involved in the sale
+ */
 async function createLightspeedSale(authToken, cards) {
     try {
         const url = `https://api.lightspeedapp.com/API/Account/${process.env.LIGHTSPEED_ACCT_ID}/Sale.json`;
@@ -118,21 +132,69 @@ async function createLightspeedSale(authToken, cards) {
     }
 }
 
+/**
+ * Creates a sale persisted to Mongo
+ *
+ * @param saleData - the data returned by Lightspeed when creating a sale
+ * @param cardList - an array of cards that were involved in the sale
+ */
+async function createSale(saleData, cardList) {
+    const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0-uytsf.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+    const client = await new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    let status;
+
+    try {
+        await client.connect();
+        console.log('Successfully connected to mongo');
+        console.log(`Creating new sale`);
+
+        const db = client.db('test');
+
+        const data = await db.collection('sales_data').insertOne({
+            sale_data: saleData,
+            card_list: cardList
+        });
+
+        status = data;
+    } catch (err) {
+        status = err;
+    } finally {
+        await client.close();
+        console.log('Disconnected from mongo');
+        return status;
+    }
+}
+
+/**
+ * Main function that wraps the execution
+ * @param {Array} cards - the cards involved in the transaction
+ */
 async function finishSale(cards) {
     try {
         const token = await request.get({
             url: process.env.REFRESH_LIGHTSPEED_AUTH_TOKEN
         });
 
+        // Grab the Lightspeed access token from response
         const { access_token } = JSON.parse(token);
 
+        // Create the Lightspeed sale
         const { data } = await createLightspeedSale(access_token, cards);
 
+        // Map updated inserts after successful Lightspeed sale creation
         const dbInserts = cards.map(async card => {
             return await updateCardInventory(card);
         });
 
+        // Persist the inventory changes
         const dbRes = await Promise.all(dbInserts);
+
+        // Create and persist sale data
+        await createSale(data.Sale, cards);
 
         return {
             cards_upserted: dbRes,

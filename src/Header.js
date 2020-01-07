38bb344e115a3d17889666b1ecb22d61a593cb41
Admin <jaipkamat@gmail.com>
2020-01-07 15:07:33 -0800

    Spiced up navbar to look more official

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import ballLogo from './logos/magic-ball.png'
+import { Menu, Icon, Dropdown } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
@@ -7,33 +8,24 @@ class Header extends React.Component {
         const { loggedIn } = this.props;
         return (
             <Menu inverted color="blue" fixed="top">
-                <Menu.Item name="Clubhouse Collection" />
+                <Menu.Item>
+                    <img src={ballLogo} style={{ marginRight: '7px' }} />
+                    <span><h3>Clubhouse Collection</h3></span>
+                </Menu.Item>
                 <Menu.Menu position="right">
-                    {loggedIn && (
-                        <React.Fragment>
-                            <Link to="/manage-inventory">
-                                <Menu.Item name="Manage Inventory" />
-                            </Link>
-                            <Link to="/new-sale">
-                                <Menu.Item name="New Sale" />
-                            </Link>
-                            <Link to="/browse-sales">
-                                <Menu.Item name="Browse Sales" />
-                            </Link>
-                        </React.Fragment>
-                    )}
-                    {!loggedIn && (
-                        <Link to="/login">
-                            <Menu.Item name="login" />
-                        </Link>
-                    )}
-                    {loggedIn && (
-                        <Link to="/logout">
-                            <Menu.Item name="logout" />
-                        </Link>
-                    )}
+                    {loggedIn && <Dropdown item icon="bars">
+                        <Dropdown.Menu>
+                            <React.Fragment>
+                                <Dropdown.Item as={Link} to="/manage-inventory"><Icon name="plus" />Manage Inventory</Dropdown.Item>
+                                <Dropdown.Item as={Link} to="/new-sale" ><Icon name="dollar sign" />New Sale</Dropdown.Item>
+                                <Dropdown.Item as={Link} to="/browse-sales" ><Icon name="eye" />Browse Sales</Dropdown.Item>
+                                <Dropdown.Item as={Link} to="/logout" ><Icon name="log out" />Logout</Dropdown.Item>
+                            </React.Fragment>
+                        </Dropdown.Menu>
+                    </Dropdown>}
+                    {!loggedIn && <Menu.Item position="right" as={Link} to="/login">Log in</Menu.Item>}
                 </Menu.Menu>
-            </Menu>
+            </Menu >
         );
     }
 }

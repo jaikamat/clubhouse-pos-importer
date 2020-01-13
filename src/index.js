1369f9da3ccdc67177d0b5800357fbfd3854a7a1
Admin <jaipkamat@gmail.com>
2020-01-12 19:49:07 -0800

    Implemented Hashrouter
    Attempting to fix serving from 'clubhouse-pos-importer' in the deployed app. Refreshing pages while navigated away from home resulted in a 404 error.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter basename='/'>
         <App />
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 

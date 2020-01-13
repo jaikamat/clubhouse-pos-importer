3fb3e0fb659592f94eb6e7804a4638d6d49d49f6
Admin <jaipkamat@gmail.com>
2020-01-13 14:23:13 -0800

    Image Zoom
    Card images now zoom on mouseover/mouse out

--- src/CardImage.js
+++ src/CardImage.js
@@ -2,22 +2,38 @@ import React from 'react';
 import { Image } from 'semantic-ui-react';
 
 const style = {
-    boxShadow: '2px 2px 5px 0 rgba(0,0,0,.25)'
+    boxShadow: '2px 2px 5px 0 rgba(0,0,0,.25)',
+    zIndex: '10',
+    transition: 'all .2s ease-in-out'
 }
 
-const CardImage = ({ image_uris, card_faces }) => {
-    try {
-        // If normal prop doesn't exist, move to catch block for flip card faces
-        return <Image src={image_uris.normal} size="tiny" style={style} />;
-    } catch (e) {
-        return <Image src={card_faces[0].image_uris.normal} size="tiny" style={style} />;
+class CardImage extends React.Component {
+    state = { hovered: false };
 
-        /**
-         * Uncomment this if they want all card faces displayed, not just the front
-         */
-        // return card_faces.map(face => {
-        //     return <Image src={face.image_uris.normal} size="tiny" />;
-        // });
+    mouseOver = () => { this.setState({ hovered: true }) };
+
+    mouseOut = () => { this.setState({ hovered: false }) };
+
+    render() {
+        const { image_uris, card_faces } = this.props
+        try {
+            // If normal prop `image_uris.normal` doesn't exist, move to catch block for flip card faces
+            return <Image
+                src={image_uris.normal}
+                size="tiny"
+                style={{ ...style, transform: `${this.state.hovered ? 'scale(1.75)' : 'scale(1)'}` }}
+                onMouseOver={this.mouseOver}
+                onMouseOut={this.mouseOut}
+            />;
+        } catch (e) {
+            return <Image src={
+                card_faces[0].image_uris.normal}
+                size="tiny"
+                style={{ ...style, transform: `${this.state.hovered ? 'scale(1.75)' : 'scale(1)'}` }}
+                onMouseOver={this.mouseOver}
+                onMouseOut={this.mouseOut}
+            />;
+        }
     }
 };
 

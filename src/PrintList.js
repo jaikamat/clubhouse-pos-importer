604d891aa56e5bbfcdbadd8a03b09b842943c1e3
Admin <jaipkamat@gmail.com>
2020-01-28 15:32:25 -0800

    Print Feature Fix:
    - Print dialog now opens text in an iframe bypassing the semantic-ui modal
    - More importantly, it is able to print to the receipt printer!

--- src/PrintList.js
+++ src/PrintList.js
@@ -1,31 +1,46 @@
 import React from 'react';
-import { Button, Modal, Icon } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 
-export default function PrintList(props) {
-    const { saleListCards } = props;
+export default class PrintList extends React.Component {
+    state = { printClicked: false }
 
-    return <React.Fragment>
-        {saleListCards.length > 0 &&
-            <Modal
-                trigger={
-                    <Button style={{ display: 'inline-block', float: 'right' }} icon>
-                        <Icon name="print" />
-                    </Button>}
-            >
-                <Modal.Header>Sale Pull List</Modal.Header>
-                <Modal.Content>
-                    <Modal.Description>
-                        <ul>
-                            {saleListCards.map(card => {
-                                return <li>{card.name} | x{card.qtyToSell} | {card.finishCondition} | {card.set_name}</li>
-                            })}
-                        </ul>
-                    </Modal.Description>
-                </Modal.Content>
-                <Modal.Actions>
-                    <Button onClick={() => window.print()}>Print me!</Button>
-                </Modal.Actions>
-            </Modal>
+    print = () => {
+        this.setState({ printClicked: true }, () => {
+            const content = document.getElementById('print-content');
+            const pri = document.getElementById('print-me').contentWindow;
+
+            pri.document.open();
+            pri.document.write(content.innerHTML);
+            pri.document.close();
+            pri.focus();
+            pri.print();
+            this.setState({ printClicked: false });
+        });
+
+    }
+
+    render() {
+        const { saleListCards } = this.props;
+
+        if (saleListCards.length > 0) { // Ensure print is hidden if no cards in list
+            return <React.Fragment>
+                {this.state.printClicked ?
+                    <iframe title="print-list" id="print-me" style={{ height: '0px', width: '0px', position: 'absolute' }}></iframe> : null
+                }
+
+                <Button style={{ display: 'inline-block', float: 'right' }} onClick={this.print} icon>
+                    <Icon name="print" />
+                </Button>
+
+                <ul id="print-content" style={{ display: 'none' }}>
+                    {saleListCards.map(card => {
+                        return <li key={card.id}>{card.name} | x{card.qtyToSell} | {card.finishCondition} | {card.set_name}</li>
+                    })}
+                </ul>
+
+            </React.Fragment >
+        } else {
+            return null;
         }
-    </React.Fragment>
+    }
 }

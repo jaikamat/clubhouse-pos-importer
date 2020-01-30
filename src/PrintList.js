0a06a49a4ed75414040d57f6810f361e46c851cd
Admin <jaipkamat@gmail.com>
2020-01-30 15:12:51 -0800

    Print Fix:
    - Added lines to print subtotal and pricing to line-items on receipt

--- src/PrintList.js
+++ src/PrintList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button, Icon } from 'semantic-ui-react';
+import Price from './Price';
 import './PrintList.css';
+import SalePriceTotal from './SalePriceTotal';
 
 export default class PrintList extends React.Component {
     state = { printClicked: false }
@@ -25,8 +27,10 @@ export default class PrintList extends React.Component {
 
                     <div id="printme" style={{ display: printClicked ? 'inline-block' : 'none' }}>
                         <ul>
-                            {saleListCards.map(c => <li key={c.id}><b>{c.name} | x{c.qtyToSell} | {c.finishCondition} | {c.set_name}</b></li>)}
+                            {saleListCards.map(c => <li key={c.id}><b>{c.name} | x{c.qtyToSell} | {c.finishCondition} | {c.set_name} | <Price num={c.price} /></b></li>)}
                         </ul>
+
+                        Subtotal: <SalePriceTotal saleList={saleListCards} />
                     </div>
                 </React.Fragment>
             )

b1de4ab7a4daabf9db33eefb7bc5befa1744649c
Admin <jaipkamat@gmail.com>
2020-01-30 14:27:38 -0800

    Print Feature Fix:
    - Print feature does not use an `<iframe>` to bypass ad-blocker
    - Scales text for receipt printer

--- src/PrintList.js
+++ src/PrintList.js
@@ -1,44 +1,35 @@
 import React from 'react';
 import { Button, Icon } from 'semantic-ui-react';
+import './PrintList.css';
 
 export default class PrintList extends React.Component {
     state = { printClicked: false }
 
     print = () => {
         this.setState({ printClicked: true }, () => {
-            const content = document.getElementById('print-content');
-            const pri = document.getElementById('print-me').contentWindow;
-
-            pri.document.open();
-            pri.document.write(content.innerHTML);
-            pri.document.close();
-            pri.focus();
-            pri.print();
-            this.setState({ printClicked: false });
+            window.print();
+            this.setState({ printClicked: false })
         });
-
     }
 
     render() {
         const { saleListCards } = this.props;
+        const { printClicked } = this.state;
 
         if (saleListCards.length > 0) { // Ensure print is hidden if no cards in list
-            return <React.Fragment>
-                {this.state.printClicked ?
-                    <iframe title="print-list" id="print-me" style={{ height: '0px', width: '0px', position: 'absolute' }}></iframe> : null
-                }
+            return (
+                <React.Fragment>
+                    <Button style={{ display: 'inline-block', float: 'right' }} onClick={this.print} icon>
+                        <Icon name="print" />
+                    </Button>
 
-                <Button style={{ display: 'inline-block', float: 'right' }} onClick={this.print} icon>
-                    <Icon name="print" />
-                </Button>
-
-                <ul id="print-content" style={{ display: 'none' }}>
-                    {saleListCards.map(card => {
-                        return <li key={card.id}>{card.name} | x{card.qtyToSell} | {card.finishCondition} | {card.set_name}</li>
-                    })}
-                </ul>
-
-            </React.Fragment >
+                    <div id="printme" style={{ display: printClicked ? 'inline-block' : 'none' }}>
+                        <ul>
+                            {saleListCards.map(c => <li key={c.id}><b>{c.name} | x{c.qtyToSell} | {c.finishCondition} | {c.set_name}</b></li>)}
+                        </ul>
+                    </div>
+                </React.Fragment>
+            )
         } else {
             return null;
         }

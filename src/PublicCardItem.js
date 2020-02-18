d2da89cfee8edf3c3858f5cb059e1557b8898320
Admin <jaipkamat@gmail.com>
2020-02-17 17:26:29 -0800

    UI Fix - Public Inventory
    - Inventory quantities no longer cover card's bottom text
    - Refactored component

--- src/PublicCardItem.js
+++ src/PublicCardItem.js
@@ -4,56 +4,45 @@ import { Image } from 'semantic-ui-react';
 import MarketPrice from './MarketPrice';
 
 const wrapperStyle = {
-    position: 'relative',
     display: 'inline-block',
+    margin: '10px 10px 10px 10px'
 };
 
-const overlayStyle = {
-    backgroundColor: 'rgba(0,0,0,0.8)',
+const inventoryStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     padding: '9px 9px 9px 9px',
-    zIndex: '1',
-    borderRadius: '10px 10px 0 0',
-    position: 'absolute',
-    width: '300px',
-    bottom: '0'
+    backgroundColor: 'rgba(0, 0, 0, 0.9)',
+    borderRadius: '10px 10px 10px 10px',
+    boxShadow: '2px 2px 5px 0 rgba(0, 0, 0, 0.25)'
 };
 
-const inlineBlock = {
-    display: 'inline-block',
-};
-
-const floatRight = {
-    display: 'inline-block',
-    float: 'right'
-};
-
-const roundedStyle = {
+const imageStyle = {
+    width: '300px', // Width of the image when size="medium"
+    height: '418.3px', // Height of the image when size="medium"
+    boxShadow: '2px 2px 5px 0 rgba(0, 0, 0, 0.25)',
+    background: 'repeating-linear-gradient(45deg, #bfbfbf, #bfbfbf 10px, #b0b0b0 10px, #b0b0b0 20px)',
     borderRadius: '15px'
 }
 
-const image = (image_uris, card_faces) => {
+export default function PublicCardItem({ image_uris, card_faces, qoh, id }) {
+    let myImage;
+
     try {
         // If normal prop doesn't exist, move to catch block for flip card faces
-        return <Image src={image_uris.normal} size="medium" style={roundedStyle} />;
+        myImage = <Image src={image_uris.normal} size="medium" style={{ borderRadius: '15px' }} />
     } catch (e) {
-        return <Image src={card_faces[0].image_uris.normal} size="medium" style={roundedStyle} />;
+        myImage = <Image src={card_faces[0].image_uris.normal} size="medium" style={{ borderRadius: '15px' }} />
     }
-};
 
-export default function PublicCardItem({ image_uris, card_faces, qoh, id }) {
-    return (
-        <React.Fragment>
-            <div style={wrapperStyle}>
-                {image(image_uris, card_faces)}
-                <div style={overlayStyle}>
-                    <div style={inlineBlock}>
-                        <QohParser inventoryQty={qoh} />
-                    </div>
-                    <div style={floatRight}>
-                        <MarketPrice id={id} />
-                    </div>
-                </div>
+    return <React.Fragment>
+        <div style={wrapperStyle}>
+            <div style={imageStyle}>{myImage}</div>
+            <div style={inventoryStyle}>
+                <div><QohParser inventoryQty={qoh} /></div>
+                <MarketPrice id={id} />
             </div>
-        </React.Fragment>
-    )
+        </div>
+    </React.Fragment>
 }

41565a0411f0b10a44a3d44bf79d0145a07158db
Admin <jaipkamat@gmail.com>
2020-03-11 20:20:21 -0400

    Refactor: AuthProvider
    Why?
    - To learn more about the Context API and make the codebase slimmer using Hooks
    
    What
    - Refactored the AuthProvider to use React Hooks. Notably slimmed down the old `state` object

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -1,55 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import makeAuthHeader from './makeAuthHeader';
 import { LOGIN } from './api_resources';
 import axios from 'axios';
 
 export const AuthContext = React.createContext();
 
-class AuthProvider extends React.Component {
-    state = {
-        loggedIn: !!localStorage.getItem('clubhouse_JWT'),
-        handleLogin: async (username, password) => {
-            try {
-                const { data } = await axios.post(LOGIN, {
-                    username: username.toLowerCase(),
-                    password: password
-                }, { headers: makeAuthHeader() });
+export default function AuthProvider(props) {
+    const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem('clubhouse_JWT'));
 
-                if (data.token) {
-                    localStorage.setItem('clubhouse_JWT', data.token);
+    const handleLogin = async (username, password) => {
+        try {
+            const { data } = await axios.post(LOGIN, {
+                username: username.toLowerCase(),
+                password: password
+            }, { headers: makeAuthHeader() });
 
-                    this.setState({
-                        loggedIn: !!localStorage.getItem('clubhouse_JWT')
-                    });
+            if (data.token) {
+                localStorage.setItem('clubhouse_JWT', data.token);
+                setLoggedIn(!!localStorage.getItem('clubhouse_JWT'))
 
-                    return { authed: true };
-                } else {
-                    return { authed: false };
-                }
-            } catch (err) {
-                console.log(err);
-            }
-        },
-        handleLogout: async () => {
-            try {
-                localStorage.removeItem('clubhouse_JWT');
-
-                this.setState({
-                    loggedIn: !!localStorage.getItem('clubhouse_JWT')
-                });
-
+                return { authed: true };
+            } else {
                 return { authed: false };
-            } catch (err) {
-                console.log(err);
             }
+        } catch (err) {
+            console.log(err);
         }
     }
 
-    render() {
-        return <AuthContext.Provider value={this.state}>
-            {this.props.children}
-        </AuthContext.Provider>
-    }
-}
+    const handleLogout = async () => {
+        try {
+            localStorage.removeItem('clubhouse_JWT');
+            setLoggedIn(!!localStorage.getItem('clubhouse_JWT'));
 
-export default AuthProvider
+            return { authed: false };
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return <AuthContext.Provider value={{ loggedIn, handleLogin, handleLogout }}>
+        {props.children}
+    </AuthContext.Provider>
+}

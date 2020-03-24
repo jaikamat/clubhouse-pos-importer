5edfed97b3b5f65ed3a2c04ac572840e5a84d19c
Admin <jaipkamat@gmail.com>
2020-03-24 12:04:26 -0700

    API Feature - GET all sales:
    Why?
    - Used for reporting and to massage data into a reasonable, manageable response client-side
    
    What
    - Created a test and production endpoint to grab all sales data, and relevant properties on them therein

--- gc_functions/production/getSales.js
+++ gc_functions/production/getSales.js
@@ -43,12 +43,72 @@ async function getSales(cardName) {
         return await data.toArray();
     } catch (err) {
         console.log(err);
-        return err;
+        throw err;
     } finally {
         await client.close();
         console.log('Disconnected from mongo');
     }
 }
+
+async function getAllSales() {
+    const client = await new MongoClient(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    try {
+        await client.connect();
+        const db = client.db(DATABASE_NAME);
+
+        const data = await db.collection('sales_data').find({}).project({
+            sale_data: 1,
+            "sale_data.total": 1,
+            "sale_data.saleID": 1,
+            "sale_data.timeStamp": 1,
+            card_list: 1,
+            "card_list.foil": 1,
+            "card_list.nonfoil": 1,
+            "card_list.id": 1,
+            "card_list.name": 1,
+            "card_list.set": 1,
+            "card_list.set_name": 1,
+            "card_list.rarity": 1,
+            "card_list.price": 1,
+            "card_list.finishCondition": 1,
+            "card_list.reserved": 1,
+            "card_list.qtyToSell": 1,
+            "card_list.card_faces": 1
+        });
+
+        const docs = await data.toArray();
+
+        // Some data, like 'total' and 'price' should be coerced to a Number for ease of use on the frontend
+        docs.forEach(el => {
+            el.sale_data.total = Number(el.sale_data.total);
+
+            el.card_list.forEach((el, idx, arr) => {
+                arr[idx].price = Number(arr[idx].price);
+            })
+        });
+
+        return docs;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    } finally {
+        await client.close();
+    }
+}
+
+app.get('/sales/', async (req, res) => {
+    try {
+        const message = await getAllSales();
+        res.status(200).send(message);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
 
 app.get('/', async (req, res) => {
     try {

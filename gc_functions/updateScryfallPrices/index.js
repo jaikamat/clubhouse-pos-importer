ee6a4d1ab02465b105064f6094ce0a2acfafe5ea
Admin <jaipkamat@gmail.com>
2020-05-27 17:42:17 -0700

    Scraping card prices in batches rather than all at once

--- gc_functions/updateScryfallPrices/index.js
+++ gc_functions/updateScryfallPrices/index.js
@@ -25,12 +25,16 @@ async function init() {
 
         console.log(`Preparing to fetch prices for ${cardIds.length} cards`);
 
-        const prices = await fetchAllPrices(cardIds);
-
-        // If some requests fail, their array values will be null and will block the mongo import - remove them
-        const filteredResults = prices.filter(d => d !== null);
-
-        await mongoBulkImport(filteredResults, database);
+        while (cardIds.length > 0) {
+            try {
+                const batchIds = cardIds.splice(0, 100);
+                const prices = await fetchAllPrices(batchIds);
+                const filteredResults = prices.filter(d => d !== null);
+                await mongoBulkImport(filteredResults, database);
+            } catch (e) { // Squelch breaking the loop if a card errors
+                console.log(e)
+            }
+        }
 
         console.timeEnd('updatePrices');
         console.log(`Prices updated for database ${database} on ${new Date().toISOString()}`);

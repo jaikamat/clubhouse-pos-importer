6e18f0c2373973baca25ea988b924de0977efb98
Admin <jaipkamat@gmail.com>
2020-06-16 08:57:36 -0700

    UI Enhancement: Toast padding
    - Padded all toast notifications out 10px from the bottom-right
    
    Closes #31

--- src/common/createToast.js
+++ src/common/createToast.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Message } from 'semantic-ui-react';
 import toaster from 'toasted-notes';
+import styled from 'styled-components';
+
+const ToastContainer = styled(Message)`
+    position: relative;
+    right: 10px;
+    bottom: 10px;
+`;
 
 const createToast = ({ color, header, message, duration = 2000 }) => {
     return toaster.notify(
         () => (
-            <Message color={color} compact>
+            <ToastContainer color={color} compact>
                 <Message.Header>{header}</Message.Header>
                 {message}
-            </Message>
+            </ToastContainer>
         ),
         {
             position: 'bottom-right',

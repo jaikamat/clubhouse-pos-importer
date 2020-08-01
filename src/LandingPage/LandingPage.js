0ba657d8e1bb1687ecd47ae677cfce8c60a0ff88
Admin <jaipkamat@gmail.com>
2020-08-01 11:14:50 -0700

    Content updates

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -4,7 +4,6 @@ import { Icon, Label } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import Card from './Card';
 import clubhouse_products from './clubhouse_products.png';
-import ikoria_splash from './ikoria_splash.jpg';
 import core_2021_draft_booster from './core_2021_draft_booster.png';
 import core_2021_collector from './core_2021_collector.jpg';
 import double_masters from './double_masters.png';
@@ -57,7 +56,13 @@ const Image2 = styled.div`
 `;
 
 const ImageMask = styled.div`
-    background-image: linear-gradient(to bottom, rgba(32, 106, 208, 1), rgba(32, 106, 208, 0.85), rgba(32, 106, 208, 0.66), rgba(255, 255, 255, 1));
+    background-image: linear-gradient(
+        to bottom,
+        rgba(32, 106, 208, 1),
+        rgba(32, 106, 208, 0.85),
+        rgba(32, 106, 208, 0.66),
+        rgba(255, 255, 255, 1)
+    );
     position: absolute;
     top: 0px;
     left: 0px;
@@ -67,7 +72,11 @@ const ImageMask = styled.div`
 `;
 
 const ImageMask2 = styled.div`
-    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(255, 255, 255, 1));
+    background-image: linear-gradient(
+        to bottom,
+        rgba(0, 0, 0, 0),
+        rgba(255, 255, 255, 1)
+    );
     position: absolute;
     left: 0px;
     height: 400px;
@@ -150,7 +159,7 @@ const Button = styled.button`
     font-size: 17px;
     color: white;
     border: 0px;
-    
+
     :hover {
         background-color: rgb(35, 139, 219);
         box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.4);
@@ -178,14 +187,14 @@ const FooterIcon = styled(Icon)`
 `;
 
 const BodyText = styled.p`
-    font-size: ${props => props.size || '16px'};
-    color: ${props => props.color || 'black'};
-    text-align: ${props => props.align === 'center' ? 'center' : 'left'};
+    font-size: ${(props) => props.size || '16px'};
+    color: ${(props) => props.color || 'black'};
+    text-align: ${(props) => (props.align === 'center' ? 'center' : 'left')};
 `;
 
 const Spacer = styled.div`
     width: 100%;
-    height: ${props => props.height || 0}px;
+    height: ${(props) => props.height || 0}px;
 `;
 
 const FooterStyle = styled.div`
@@ -206,157 +215,234 @@ const SocialMedia = styled.div`
     right: 10px;
 `;
 
-const ServiceCard = ({ icon = "users", header = "Header", content = "Content", align }) => {
-    return <FlexColumn>
-        <IconStyled circular name={icon} size="huge" />
-        <Subheader>{header}</Subheader>
-        <BodyText color="gray" align={align}>
-            {content}
-        </BodyText>
-    </FlexColumn>
-}
+const ServiceCard = ({
+    icon = 'users',
+    header = 'Header',
+    content = 'Content',
+    align,
+}) => {
+    return (
+        <FlexColumn>
+            <IconStyled circular name={icon} size="huge" />
+            <Subheader>{header}</Subheader>
+            <BodyText color="gray" align={align}>
+                {content}
+            </BodyText>
+        </FlexColumn>
+    );
+};
 
-const InfoCard = ({ icon = "users", header = "Header", content = "Content", align }) => {
-    return <FlexColumn>
-        <Icon name={icon} size="big" />
-        <Subheader>{header}</Subheader>
-        <BodyText color="gray" align={align}>
-            {content}
-        </BodyText>
-    </FlexColumn>
-}
+const InfoCard = ({
+    icon = 'users',
+    header = 'Header',
+    content = 'Content',
+    align,
+}) => {
+    return (
+        <FlexColumn>
+            <Icon name={icon} size="big" />
+            <Subheader>{header}</Subheader>
+            <BodyText color="gray" align={align}>
+                {content}
+            </BodyText>
+        </FlexColumn>
+    );
+};
 
 export default function LandingPage() {
-    return <Wrapper>
-        <BaseContent>
-            <ContentContainer>
-                <SocialMedia>
-                    <div>
-                        <a href="https://discord.gg/Wpej7dQ" target="_blank" rel="noopener noreferrer">
-                            <FooterIcon link name="discord" size="big" />
-                        </a>
-                        <a href="https://www.facebook.com/RGTClubhouse/" target="_blank" rel="noopener noreferrer">
-                            <FooterIcon link name="facebook" size="big" />
-                        </a>
-                    </div>
-                </SocialMedia>
-                <Spacer height="30" />
-                <Divider>
-                    <Slogan>The Clubhouse is open <span role="img" aria-label="confetti">🎉</span></Slogan>
-                    <Subheader color="white">We are open 7 days a week! Play space use and events are cancelled until further notice due to WPN's in-store play suspension.</Subheader>
-                    <FlexRowResponsive>
-                        {/* <Link to="/calendar">
+    return (
+        <Wrapper>
+            <BaseContent>
+                <ContentContainer>
+                    <SocialMedia>
+                        <div>
+                            <a
+                                href="https://discord.gg/Wpej7dQ"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FooterIcon link name="discord" size="big" />
+                            </a>
+                            <a
+                                href="https://www.facebook.com/RGTClubhouse/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <FooterIcon link name="facebook" size="big" />
+                            </a>
+                        </div>
+                    </SocialMedia>
+                    <Spacer height="30" />
+                    <Divider>
+                        <Slogan>
+                            The Clubhouse is open{' '}
+                            <span role="img" aria-label="confetti">
+                                🎉
+                            </span>
+                        </Slogan>
+                        <Subheader color="white">
+                            We are open Tuesday through Sunday for all your CCG
+                            needs!{' '}
+                            <em>
+                                (Play space use and events are cancelled until
+                                further notice due to WPN's in-store play
+                                suspension)
+                            </em>
+                        </Subheader>
+                        <FlexRowResponsive>
+                            {/* <Link to="/calendar">
                             <Button><b>Events calendar</b></Button>
                         </Link> */}
-                        <Link to="/public-inventory">
-                            <Button><b>Browse cards</b></Button>
-                        </Link>
-                    </FlexRowResponsive>
-                    <ImageMask />
-                    <Image />
-                </Divider>
+                            <Link to="/public-inventory">
+                                <Button>
+                                    <b>Browse cards</b>
+                                </Button>
+                            </Link>
+                        </FlexRowResponsive>
+                        <ImageMask />
+                        <Image />
+                    </Divider>
 
-                <FlexRow>
-                    <Header>Latest News</Header>
-                </FlexRow>
-                <Spacer height="30" />
-                <Grid>
-                    <Card imageSrc={double_masters}
-                        header="New product preorders!"
-                        date="06/08/2020">
-                        <p>
-                            The time has come! We have had loads of questions about all the upcoming products, and we are excited to start taking PRE ORDERS on a range of upcoming items:
-                        </p>
-                        <ul>
-                            <li>Jump Start $90 per box, Limit 2</li>
-                            <li>Double Masters $249 per box, Limit 2</li>
-                            <li>Double Masters VIP $369 per box, No Limit</li>
-                            <li>Double Master VIP $95 per pack, Limit 1</li>
-                        </ul>
-                    </Card>
-                    <Card imageSrc={core_2021_collector}
-                        header="Collector Booster preorders!"
-                        date="06/02/2020">
-                        <p>
-                            To compound the excitement, We JUST got numbers on Collector Boosters for Core 2021! We are now taking pre-orders for those as well! $229 per, limit 1 box per customer.
-                        </p>
-                    </Card>
-                    <Card imageSrc={core_2021_draft_booster}
-                        header="Core 2021 preorders!"
-                        date="06/02/2020">
-                        <p>
-                            It's already time. Core 2021 is upon us. We are now taking Booster Box pre-orders! $100 per box, 2 boxes per person limit. Pre-release sign ups will begin on June 19th!
-                        </p>
-                        <p>
-                            Note: Preorders for draft booster boxes have now ended. We currently have them for sale at $110 each. Collector Booster preorders will end on June 3rd.
-                        </p>
-                    </Card>
-                </Grid>
-
-                <Spacer height="30" />
-
-                <Divider>
-                    <Spacer height="30" />
                     <FlexRow>
-                        <Header>Our Services</Header>
-                    </FlexRow>
-                    <FlexRow>
-                        <Subheader>We're here for all your CCG needs!</Subheader>
+                        <Header>Latest News</Header>
                     </FlexRow>
                     <Spacer height="30" />
                     <Grid>
-                        <ServiceCard
-                            icon="box"
-                            header="Large product selection"
-                            content="We stock an extensive selection of singles and CCG supplies just for you! When the deckbuilding itch strikes, turn to us"
+                        <Card
+                            imageSrc={double_masters}
+                            header="New product preorders!"
+                            date="06/08/2020"
+                        >
+                            <p>
+                                <em>
+                                    Note: Preorders for Double Masters are now
+                                    closed.
+                                </em>
+                            </p>
+                            <p>
+                                The time has come! We have had loads of
+                                questions about all the upcoming products, and
+                                we are excited to start taking PRE ORDERS on a
+                                range of upcoming items:
+                            </p>
+                            <ul>
+                                <li>Jump Start $90 per box, Limit 2</li>
+                                <li>Double Masters $249 per box, Limit 2</li>
+                                <li>
+                                    Double Masters VIP $369 per box, No Limit
+                                </li>
+                                <li>Double Master VIP $95 per pack, Limit 1</li>
+                            </ul>
+                        </Card>
+                        <Card
+                            imageSrc={core_2021_collector}
+                            header="Collector Booster preorders!"
+                            date="06/02/2020"
+                        >
+                            <p>
+                                To compound the excitement, We JUST got numbers
+                                on Collector Boosters for Core 2021! We are now
+                                taking pre-orders for those as well! $229 per,
+                                limit 1 box per customer.
+                            </p>
+                        </Card>
+                        <Card
+                            imageSrc={core_2021_draft_booster}
+                            header="Core 2021 preorders!"
+                            date="06/02/2020"
+                        >
+                            <p>
+                                It's already time. Core 2021 is upon us. We are
+                                now taking Booster Box pre-orders! $100 per box,
+                                2 boxes per person limit. Pre-release sign ups
+                                will begin on June 19th!
+                            </p>
+                            <p>
+                                Note: Preorders for draft booster boxes have now
+                                ended. We currently have them for sale at $110
+                                each. Collector Booster preorders will end on
+                                June 3rd.
+                            </p>
+                        </Card>
+                    </Grid>
+
+                    <Spacer height="30" />
+
+                    <Divider>
+                        <Spacer height="30" />
+                        <FlexRow>
+                            <Header>Our Services</Header>
+                        </FlexRow>
+                        <FlexRow>
+                            {/* <Subheader>
+                                We're here for all your CCG needs!
+                            </Subheader> */}
+                        </FlexRow>
+                        <Spacer height="30" />
+                        <Grid>
+                            <ServiceCard
+                                icon="box"
+                                header="Large product selection"
+                                content="We stock an extensive selection of singles and CCG supplies just for you! When the deckbuilding itch strikes, turn to us"
+                                align="center"
+                            />
+                            <ServiceCard
+                                header="Inclusive community"
+                                content="We pride ourselves on our friendly atmosphere. Young or seasoned, beginner or advanced, we welcome all players!"
+                                align="center"
+                            />
+                            <ServiceCard
+                                icon="dollar sign"
+                                header="Cash or credit trade-ins"
+                                content="Whether a whole collection or a single card, come on by and have one of our expert managers appraise it using competitive market rates"
+                                align="center"
+                            />
+                        </Grid>
+                        <ImageMask2 />
+                        <Image2 />
+                    </Divider>
+                    <Spacer height="30" />
+                    <Grid>
+                        <InfoCard
+                            icon="clock"
+                            header="Hours"
+                            content="Tues - Fri 2pm to 7pm, Sat - Sun 12pm to 7pm"
                             align="center"
                         />
-                        <ServiceCard
-                            header="Inclusive community"
-                            content="We pride ourselves on our friendly atmosphere. Young or seasoned, beginner or advanced, we welcome all players!"
+                        <InfoCard
+                            icon="location arrow"
+                            header="Location"
+                            content="13895 SW Farmington Rd, Beaverton, OR 97005"
                             align="center"
                         />
-                        <ServiceCard
-                            icon="dollar sign"
-                            header="Cash or credit trade-ins"
-                            content="Whether a whole collection or a single card, come on by and have one of our expert managers appraise it using competitive market rates"
-                            align="center"
+                        <InfoCard
+                            icon="phone"
+                            header="Contact"
+                            content="(503) 268-1449"
                         />
                     </Grid>
-                    <ImageMask2 />
-                    <Image2 />
-                </Divider>
-                <Spacer height="30" />
-                <Grid>
-                    <InfoCard
-                        icon="clock"
-                        header="Hours"
-                        content="Mon - Fri 2pm to 7pm, Sat - Sun 12pm to 7pm"
-                        align="center"
-                    />
-                    <InfoCard
-                        icon="location arrow"
-                        header="Location"
-                        content="13895 SW Farmington Rd, Beaverton, OR 97005"
-                        align="center"
-                    />
-                    <InfoCard
-                        icon="phone"
-                        header="Contact"
-                        content="(503) 268-1449"
-                    />
-                </Grid>
-                <Spacer height="30" />
-            </ContentContainer>
-        </BaseContent>
-        <FooterStyle>
-            <SpanStyle>
-                <Link to="/login" id="login-btn">
-                    <Label as='a' color="black">Staff login</Label>
-                </Link>
-            </SpanStyle>
-            <SpanStyle><BodyText size="12px" color="white">© 2020 The Clubhouse</BodyText></SpanStyle>
-            <SpanStyle><BodyText size="12px" color="gray">Powered by Scryfall</BodyText></SpanStyle>
-        </FooterStyle>
-    </Wrapper>
+                    <Spacer height="30" />
+                </ContentContainer>
+            </BaseContent>
+            <FooterStyle>
+                <SpanStyle>
+                    <Link to="/login" id="login-btn">
+                        <Label as="a" color="black">
+                            Staff login
+                        </Label>
+                    </Link>
+                </SpanStyle>
+                <SpanStyle>
+                    <BodyText size="12px" color="white">
+                        © 2020 The Clubhouse
+                    </BodyText>
+                </SpanStyle>
+                <SpanStyle>
+                    <BodyText size="12px" color="gray">
+                        Powered by Scryfall
+                    </BodyText>
+                </SpanStyle>
+            </FooterStyle>
+        </Wrapper>
+    );
 }

5c1e7dc0767defdcf347e2efe07ab475542fe564
Admin <jaipkamat@gmail.com>
2020-08-01 14:11:24 -0700

    Consolidated Main and App, deleted Main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import Main from './Main';
 import Header from './Header/Header';
 import AuthProvider from './context/AuthProvider';
+import { Switch, Route } from 'react-router-dom';
+import Home from './ManageInventory/Home';
+import Sale from './NewSale/Sale';
+import BrowseSales from './BrowseSales/BrowseSales';
+import PublicInventory from './PublicInventory/PublicInventory';
+import DeckboxClone from './DeckboxClone/DeckboxClone';
+import Login from './Login/Login';
+import Logout from './Logout/Logout';
+import Receiving from './Receiving/Receiving';
+import Reports from './Reports/Reports';
+import LandingPage from './LandingPage/LandingPage';
+import styled from 'styled-components';
 import { SaleProvider } from './context/SaleContext';
 import { ReceivingProvider } from './context/ReceivingContext';
+
+const ContentContainer = styled.div`
+    padding-top: 75px;
+    margin-left: 20px;
+    margin-right: 20px;
+`;
+
+const BackgroundColor = styled.div`
+    background-color: #f9fafb;
+    min-height: 100vh;
+`;
 
 class App extends React.Component {
     render() {
@@ -12,7 +34,66 @@ class App extends React.Component {
                 <Header />
                 <SaleProvider>
                     <ReceivingProvider>
-                        <Main />
+                        <Switch>
+                            <ReceivingProvider>
+                                <SaleProvider>
+                                    <Route
+                                        exact
+                                        path="/"
+                                        component={LandingPage}
+                                    />
+                                    <BackgroundColor>
+                                        <ContentContainer id="content-container">
+                                            <Route
+                                                exact
+                                                path="/manage-inventory"
+                                                component={Home}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/new-sale"
+                                                component={Sale}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/browse-sales"
+                                                component={BrowseSales}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/browse-inventory"
+                                                component={DeckboxClone}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/public-inventory"
+                                                component={PublicInventory}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/receiving"
+                                                component={Receiving}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/reports"
+                                                component={Reports}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/login"
+                                                component={Login}
+                                            />
+                                            <Route
+                                                exact
+                                                path="/logout"
+                                                component={Logout}
+                                            />
+                                        </ContentContainer>
+                                    </BackgroundColor>
+                                </SaleProvider>
+                            </ReceivingProvider>
+                        </Switch>
                     </ReceivingProvider>
                 </SaleProvider>
             </AuthProvider>

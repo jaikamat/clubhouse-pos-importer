fd3b584c7df6ff76086c6c0dfe0a0bcf3b52a7bd
Admin <jaipkamat@gmail.com>
2020-10-04 13:37:38 -0700

    Migrating login

--- monolith/routes/index.js
+++ monolith/routes/index.js
@@ -1,9 +1,69 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const DATABASE_NAME = 'test';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+/**
+ * Initialize express app and use CORS middleware
+ */
+const app = express();
+
+async function getJwt(username, submittedPass) {
+    const client = await new MongoClient(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    try {
+        await client.connect();
+        console.log('Successfully connected to mongo');
+
+        const db = client.db(DATABASE_NAME);
+
+        const user = await db.collection('users').findOne({
+            username: username,
+        });
+
+        if (!user) return 'Not authorized';
+
+        // Determine if the fetched user is authorized
+        const match = await bcrypt.compareSync(submittedPass, user.password);
+
+        if (match) {
+            const token = jwt.sign(
+                { username: username, admin: true },
+                process.env.PRIVATE_KEY
+            );
+
+            return { token: token };
+        } else {
+            return 'Not authorized';
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    } finally {
+        await client.close();
+        console.log('Disconnected from Mongo');
+    }
+}
+
+router.post('/', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const token = await getJwt(username, password);
+        res.status(200).send(token);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
 });
 
 module.exports = router;

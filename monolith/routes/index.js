9e636bf0a38372f8f682424f88183a4ca37a65fe
Admin <jaipkamat@gmail.com>
2020-10-04 14:24:41 -0700

    moved getCardsWithInfo

--- monolith/routes/index.js
+++ monolith/routes/index.js
@@ -112,4 +112,93 @@ router.post('/getCardQuantitiesFromInventory', async (req, res) => {
     }
 });
 
+async function getCardsWithInfo(title, matchInStock = false) {
+    // if matchInStock is false, we get all cards, even those with no stock
+    const mongoConfig = { useNewUrlParser: true, useUnifiedTopology: true };
+
+    try {
+        var client = await new MongoClient(
+            process.env.MONGO_URI,
+            mongoConfig
+        ).connect();
+        console.log('Successfully connected to mongo');
+
+        const db = client.db(DATABASE_NAME);
+
+        const pipeline = [];
+
+        // Fetch by title
+        const match = {
+            $match: {
+                name: title,
+                games: {
+                    $in: ['paper'],
+                },
+            },
+        };
+
+        // Zip up bulk with qoh
+        const lookup = {
+            $lookup: {
+                from: 'card_inventory',
+                localField: 'id',
+                foreignField: '_id',
+                as: 'qoh',
+            },
+        };
+
+        // $lookup yields an array - replace the new qoh with the nested card value
+        const addFields = {
+            $addFields: {
+                qoh: { $arrayElemAt: ['$qoh.qoh', 0] },
+            },
+        };
+
+        // Only yield cards that are in-stock, if needed
+        const inventoryMatch = {
+            $match: {
+                $or: [
+                    { 'qoh.FOIL_NM': { $gt: 0 } },
+                    { 'qoh.FOIL_LP': { $gt: 0 } },
+                    { 'qoh.FOIL_MP': { $gt: 0 } },
+                    { 'qoh.FOIL_HP': { $gt: 0 } },
+                    { 'qoh.NONFOIL_NM': { $gt: 0 } },
+                    { 'qoh.NONFOIL_LP': { $gt: 0 } },
+                    { 'qoh.NONFOIL_MP': { $gt: 0 } },
+                    { 'qoh.NONFOIL_HP': { $gt: 0 } },
+                ],
+            },
+        };
+
+        pipeline.push(match);
+        pipeline.push(lookup);
+        pipeline.push(addFields);
+        if (matchInStock) pipeline.push(inventoryMatch);
+
+        return await db
+            .collection('scryfall_bulk_cards')
+            .aggregate(pipeline)
+            .toArray();
+    } catch (err) {
+        console.log(err);
+        return err;
+    } finally {
+        await client.close();
+        console.log('Disconnected from mongo');
+    }
+}
+
+router.get('/getCardsWithInfo', async (req, res) => {
+    try {
+        const { title, matchInStock } = req.query;
+        const myMatch = matchInStock === 'true';
+
+        const message = await getCardsWithInfo(title, myMatch);
+        res.status(200).send(message);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+});
+
 module.exports = router;

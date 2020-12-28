2458366449908bb69dbae1a899f96b34749c1d23
Admin <jaipkamat@gmail.com>
2020-12-28 12:32:37 -0800

    refactored signin to use formik

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,9 +1,10 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import createToast from '../common/createToast';
 import { Form, Button, Segment } from 'semantic-ui-react';
 import { Redirect } from 'react-router-dom';
 import { AuthContext } from '../context/AuthProvider';
 import styled from 'styled-components';
+import { Formik } from 'formik';
 
 const LoginContainer = styled.div`
     margin-top: 15px;
@@ -16,28 +17,19 @@ const FormContainer = styled(Segment)`
     padding: 25px 25px 25px 25px !important;
 `;
 
-const initialState = { username: '', password: '', loading: false };
-
 export default function Login() {
-    const [state, setState] = useState(initialState);
     const { loggedIn, handleLogin } = useContext(AuthContext);
 
-    const handleInputChange = (e, { value }) => setState({ ...state, [e.target.name]: value });
-
-    const login = async () => {
-        setState({ loading: true });
-
-        const { authed } = await handleLogin(state.username, state.password);
+    const onSubmit = async ({ username, password }) => {
+        const { authed } = await handleLogin(username, password);
 
         if (authed) {
-            // Do not set state here to mitigate React setState warning after component unmounted due to redirect
             createToast({
                 color: 'green',
                 header: 'Success',
                 message: `Enjoy your time here!`,
             });
         } else {
-            setState(initialState);
             createToast({
                 color: 'red',
                 header: 'Error',
@@ -46,43 +38,58 @@ export default function Login() {
         }
     };
 
-    if (loggedIn) return <Redirect to="/manage-inventory" />
+    if (loggedIn) return <Redirect to="/manage-inventory" />;
 
-    return <LoginContainer>
-        <FormContainer raised loading={state.loading}>
-            <Form>
-                <Form.Field>
-                    <Form.Input
-                        className="username-input"
-                        name="username"
-                        placeholder="Username"
-                        label="Username"
-                        value={state.username || ''}
-                        onChange={handleInputChange}
-                    />
-                </Form.Field>
-                <Form.Field>
-                    <Form.Input
-                        className="password-input"
-                        name="password"
-                        placeholder="Password"
-                        type="password"
-                        label="Password"
-                        value={state.password || ''}
-                        onChange={handleInputChange}
-                    />
-                </Form.Field>
-                <Button
-                    primary
-                    fluid
-                    floated="right"
-                    type="submit"
-                    onClick={login}
-                    className="login-btn"
-                    disabled={!state.username || !state.password}>
-                    Submit
-            </Button>
-            </Form>
-        </FormContainer>
-    </LoginContainer>
+    return (
+        <LoginContainer>
+            <Formik
+                initialValues={{
+                    username: '',
+                    password: '',
+                }}
+                onSubmit={onSubmit}
+            >
+                {({ values, handleSubmit, setFieldValue, isSubmitting }) => (
+                    <FormContainer raised loading={isSubmitting}>
+                        <Form>
+                            <Form.Field>
+                                <Form.Input
+                                    className="username-input"
+                                    placeholder="Username"
+                                    label="Username"
+                                    value={values.username}
+                                    onChange={(_, { value }) =>
+                                        setFieldValue('username', value)
+                                    }
+                                />
+                            </Form.Field>
+                            <Form.Field>
+                                <Form.Input
+                                    className="password-input"
+                                    placeholder="Password"
+                                    type="password"
+                                    label="Password"
+                                    value={values.password}
+                                    onChange={(_, { value }) =>
+                                        setFieldValue('password', value)
+                                    }
+                                />
+                            </Form.Field>
+                            <Button
+                                primary
+                                fluid
+                                floated="right"
+                                type="submit"
+                                onClick={handleSubmit}
+                                className="login-btn"
+                                disabled={!values.username || !values.password}
+                            >
+                                Submit
+                            </Button>
+                        </Form>
+                    </FormContainer>
+                )}
+            </Formik>
+        </LoginContainer>
+    );
 }

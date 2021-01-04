64245b32fb3f3e570e7ae69150e12e8b86382307
Admin <jaipkamat@gmail.com>
2021-01-03 19:34:03 -0800

    Deploying to GH-Pages

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ddb4d504625b65fa1e3e32ddf00cfe92.js"
+  "/precache-manifest.8f199687d01de165de2b8e253757fe64.js"
 );
 
 self.addEventListener('message', (event) => {

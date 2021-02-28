87afc21a094c5fd148c5f6118820db1316d89261
Admin <jaipkamat@gmail.com>
2021-02-28 10:03:20 -0800

    refactor SearchBar

--- src/common/SearchBar.tsx
+++ src/common/SearchBar.tsx
@@ -1,53 +1,59 @@
-import React, { useState } from 'react';
-import { Search } from 'semantic-ui-react';
+import React, { FC, SyntheticEvent, useState } from 'react';
+import { Search, SearchProps, SearchResultData } from 'semantic-ui-react';
 import _ from 'lodash';
 import axios from 'axios';
 import makeAuthHeader from '../utils/makeAuthHeader';
 import { SCRYFALL_AUTOCOMPLETE } from '../utils/api_resources';
 import $ from 'jquery';
+import searchQuery from './searchQuery';
 
-export default function SearchBar(props) {
-    const [isLoading, setIsLoading] = useState(false);
-    const [results, setResults] = useState([]);
+interface Props {
+    handleSearchSelect: (result: string) => void;
+    onBlur?: (event: SyntheticEvent, data: SearchProps) => void;
+}
 
-    const handleSearchChange = (e, { value }) => {
-        // `value` is the search input string
+const SearchBar: FC<Props> = ({ handleSearchSelect, onBlur }) => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [results, setResults] = useState<{ title: string }[]>([]);
+
+    // `value` is the search input string
+    const handleSearchChange = (_: SyntheticEvent, { value }: SearchProps) => {
+        if (!value) return;
+
         if (value.length < 1) {
-            setIsLoading(false);
+            setLoading(false);
             setResults([]);
             return;
         }
 
         if (value.length < 3) return; // Don't search if only 2 letters in input
 
-        setIsLoading(true);
+        setLoading(true);
 
         setTimeout(async () => {
-            const { data } = await axios.get(
-                `${SCRYFALL_AUTOCOMPLETE}?q=${value}`,
-                {
-                    headers: makeAuthHeader(),
-                }
-            );
+            const data = await searchQuery(value);
 
             const formattedResults = data.data
                 .map((el) => ({ title: el }))
                 .slice(0, 7);
 
             setResults(formattedResults);
-            setIsLoading(false);
+            setLoading(false);
         }, 100);
     };
 
-    const handleResultSelect = async (e, { result }) => {
+    const handleResultSelect = async (
+        _: SyntheticEvent,
+        { result }: SearchResultData
+    ) => {
         // This line is a hacky way to get around the fact that if we just select(), then
         // when the user manually clicks the first (or any) result in the resultlist, it does not select,
         // presumably because there is some collision between selecting the resultList element and focusing the input
         setTimeout(() => $('#searchBar').select(), 10);
         try {
-            setIsLoading(true);
-            await props.handleSearchSelect(result.title);
-            setIsLoading(false);
+            setLoading(true);
+            await handleSearchSelect(result.title);
+            setLoading(false);
         } catch (e) {
             console.log(e);
         }
@@ -60,13 +66,16 @@ export default function SearchBar(props) {
                 trailing: true,
             })}
             onResultSelect={handleResultSelect}
-            loading={isLoading}
+            loading={loading}
             results={results}
             placeholder="Enter a card title"
             selectFirstResult={true}
             id="searchBar"
-            onFocus={(e) => e.target.select()}
-            onBlur={props.onBlur} // Used to clear state in the Browse Inventory feature
+            // TODO: Figure out how to select...
+            // onFocus={(e) => e.currentTarget.select()}
+            onBlur={onBlur} // Used to clear state in the Browse Inventory feature
         />
     );
-}
+};
+
+export default SearchBar;

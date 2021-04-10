8309fb0bed548685aad3bd380d09a76e89c96342
Admin <jaipkamat@gmail.com>
2021-04-10 14:40:29 -0700

    use db singleton in getCardQuantitiesFromInventory

--- monolith/interactors/getCardQuantitiesFromInventory.ts
+++ monolith/interactors/getCardQuantitiesFromInventory.ts
@@ -1,36 +1,28 @@
-import { MongoClient } from 'mongodb';
+import getDatabaseConnection from '../database';
 import collectionFromLocation from '../lib/collectionFromLocation';
-import getDatabaseName from '../lib/getDatabaseName';
 import { ClubhouseLocation } from './getJwt';
-const DATABASE_NAME = getDatabaseName();
 
 async function getCardQuantitiesFromInventory(
     scryfallIds: string[],
     location: ClubhouseLocation
 ) {
-    const client = await new MongoClient(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-
     try {
-        await client.connect();
+        const db = await getDatabaseConnection();
+        const collection = db.collection(
+            collectionFromLocation(location).cardInventory
+        );
 
-        const db = client.db(DATABASE_NAME);
-
-        const data = await db
-            .collection(collectionFromLocation(location).cardInventory)
-            .find(
-                {
-                    _id: { $in: scryfallIds },
+        const data = await collection.find(
+            {
+                _id: { $in: scryfallIds },
+            },
+            {
+                projection: {
+                    _id: true,
+                    qoh: true,
                 },
-                {
-                    projection: {
-                        _id: true,
-                        qoh: true,
-                    },
-                }
-            );
+            }
+        );
 
         const documents = await data.toArray();
 
@@ -42,8 +34,6 @@ async function getCardQuantitiesFromInventory(
     } catch (err) {
         console.log(err);
         throw err;
-    } finally {
-        await client.close();
     }
 }
 

bbf1e03dfd59f434f516430e6b058116cc45a486
Admin <jaipkamat@gmail.com>
2021-04-10 14:33:24 -0700

    use db singleton in getAllSales

--- monolith/interactors/getAllSales.ts
+++ monolith/interactors/getAllSales.ts
@@ -1,39 +1,33 @@
-import { MongoClient } from 'mongodb';
+import getDatabaseConnection from '../database';
 import collectionFromLocation from '../lib/collectionFromLocation';
-import getDatabaseName from '../lib/getDatabaseName';
-import mongoOptions from '../lib/mongoOptions';
-const DATABASE_NAME = getDatabaseName();
 import { ClubhouseLocation } from './getJwt';
 
 async function getAllSales(location: ClubhouseLocation) {
-    const client = await new MongoClient(process.env.MONGO_URI, mongoOptions);
-
     try {
-        await client.connect();
-        const db = client.db(DATABASE_NAME);
+        const db = await getDatabaseConnection();
+        const collection = db.collection(
+            collectionFromLocation(location).salesData
+        );
 
-        const data = await db
-            .collection(collectionFromLocation(location).salesData)
-            .find({})
-            .project({
-                sale_data: 1,
-                'sale_data.total': 1,
-                'sale_data.saleID': 1,
-                'sale_data.timeStamp': 1,
-                card_list: 1,
-                'card_list.foil': 1,
-                'card_list.nonfoil': 1,
-                'card_list.id': 1,
-                'card_list.name': 1,
-                'card_list.set': 1,
-                'card_list.set_name': 1,
-                'card_list.rarity': 1,
-                'card_list.price': 1,
-                'card_list.finishCondition': 1,
-                'card_list.reserved': 1,
-                'card_list.qtyToSell': 1,
-                'card_list.card_faces': 1,
-            });
+        const data = await collection.find({}).project({
+            sale_data: 1,
+            'sale_data.total': 1,
+            'sale_data.saleID': 1,
+            'sale_data.timeStamp': 1,
+            card_list: 1,
+            'card_list.foil': 1,
+            'card_list.nonfoil': 1,
+            'card_list.id': 1,
+            'card_list.name': 1,
+            'card_list.set': 1,
+            'card_list.set_name': 1,
+            'card_list.rarity': 1,
+            'card_list.price': 1,
+            'card_list.finishCondition': 1,
+            'card_list.reserved': 1,
+            'card_list.qtyToSell': 1,
+            'card_list.card_faces': 1,
+        });
 
         const docs = await data.toArray();
 
@@ -50,8 +44,6 @@ async function getAllSales(location: ClubhouseLocation) {
     } catch (err) {
         console.log(err);
         throw err;
-    } finally {
-        await client.close();
     }
 }
 

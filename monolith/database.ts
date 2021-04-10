59540594e108433b7d8bd20f9cd7581a418cffc2
Admin <jaipkamat@gmail.com>
2021-04-10 13:48:31 -0700

    first pass at creating databse connection methods

---
+++ monolith/database.ts
@@ -0,0 +1,30 @@
+import mongoOptions from './lib/mongoOptions';
+import { Db, MongoClient } from 'mongodb';
+import getDatabaseName from './lib/getDatabaseName';
+const DATABASE_NAME = getDatabaseName();
+
+let _db: Db;
+let _connection: MongoClient;
+
+export const closeConnection = async () => await _connection.close();
+
+/**
+ * Connects to mongoDB. Returns a DB instance as a singleton for passing around
+ */
+const connectToDatabase = async () => {
+    if (_db) return _db;
+
+    console.log('Attempting to connect to database...');
+
+    const client = new MongoClient(process.env.MONGO_URI, mongoOptions);
+
+    _connection = await client.connect();
+
+    console.log('Database connection established');
+
+    _db = await client.db(DATABASE_NAME);
+
+    return _db;
+};
+
+export default connectToDatabase;

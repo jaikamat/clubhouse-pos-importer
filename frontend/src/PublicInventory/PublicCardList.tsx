5b2bb21b88159f1ab0f811098a5003b118076ee5
Admin <jaipkamat@gmail.com>
2021-05-19 21:50:08 -0700

    fix publicInentory styling

--- frontend/src/PublicInventory/PublicCardList.tsx
+++ frontend/src/PublicInventory/PublicCardList.tsx
@@ -1,26 +1,26 @@
 import React, { FC } from 'react';
 import PublicCardItem from './PublicCardItem';
-import { Grid } from 'semantic-ui-react';
 import { InventoryCard } from '../utils/ScryfallCard';
+import styled from 'styled-components';
 
 interface Props {
     cards: InventoryCard[];
 }
 
-const gridStyle = {
-    display: 'flex',
-    flexWrap: 'wrap',
-    alignContent: 'space-evenly',
-    height: '100%',
-};
+const GridContainer = styled('div')({
+    display: 'grid',
+    gridGap: '10px',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+    justifyItems: 'center',
+});
 
 const PublicCardList: FC<Props> = ({ cards }) => {
     return (
-        <Grid style={gridStyle}>
+        <GridContainer>
             {cards.map((c) => (
                 <PublicCardItem key={c.id} card={c} />
             ))}
-        </Grid>
+        </GridContainer>
     );
 };
 

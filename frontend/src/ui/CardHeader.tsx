98f53ebe0f2a46d84b310166a05e766044b59cf8
Admin <jaipkamat@gmail.com>
2021-06-13 10:19:05 -0700

    created outgoing tcg link

--- frontend/src/ui/CardHeader.tsx
+++ frontend/src/ui/CardHeader.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { InventoryCard } from '../utils/ScryfallCard';
-import { Label, Item } from 'semantic-ui-react';
+import { Label, Item, Button, Icon } from 'semantic-ui-react';
 import QohLabels from '../common/QohLabels';
 import Language from '../common/Language';
 import MarketPrice from '../common/MarketPrice';
@@ -18,13 +18,60 @@ const SetIcon = styled('i')({
     fontSize: '30px',
 });
 
+// TODO: remove this shim after TCG api approval and integration
+const TcgPriceButton: FC<{ tcgId: number | null }> = ({ tcgId }) => {
+    const tcgUrl = `https://www.tcgplayer.com/product/${tcgId}`;
+
+    if (!tcgId) {
+        return (
+            <Button
+                icon
+                disabled
+                color="twitter"
+                labelPosition="right"
+                size="mini"
+                as="a"
+                href={tcgUrl}
+                target="_blank"
+            >
+                Link unavailable
+                <Icon name="external share" />
+            </Button>
+        );
+    }
+
+    return (
+        <Button
+            icon
+            color="twitter"
+            labelPosition="right"
+            size="mini"
+            as="a"
+            href={tcgUrl}
+            target="_blank"
+        >
+            View on TCG
+            <Icon name="external share" />
+        </Button>
+    );
+};
+
 const CardHeader: FC<Props> = ({
     card,
     selectedFinish,
     showMid = false,
     round = false,
 }) => {
-    const { id, display_name, set, rarity, set_name, qoh, lang } = card;
+    const {
+        id,
+        display_name,
+        set,
+        rarity,
+        set_name,
+        qoh,
+        lang,
+        tcgplayer_id,
+    } = card;
 
     return (
         <Item.Header as="h3">
@@ -41,6 +88,7 @@ const CardHeader: FC<Props> = ({
                 round={round}
             />
             <Language languageCode={lang} />
+            <TcgPriceButton tcgId={tcgplayer_id} />
         </Item.Header>
     );
 };

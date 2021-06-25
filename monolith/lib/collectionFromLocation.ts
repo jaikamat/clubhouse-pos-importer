96ee3a5f410089038e3cf4799ef0d31168887d03
Admin <jaipkamat@gmail.com>
2021-06-25 12:58:27 -0700

    centralize collection names via enum

--- monolith/lib/collectionFromLocation.ts
+++ monolith/lib/collectionFromLocation.ts
@@ -1,19 +1,32 @@
 import { ClubhouseLocation } from '../common/types';
 
+export enum Collection {
+    cardInventory = 'card_inventory',
+    salesData = 'sales_data',
+    suspendedSales = 'suspended_sales',
+    receivedCards = 'received_cards',
+    cardInventory2 = 'card_inventory_ch2',
+    salesData2 = 'sales_data_ch2',
+    suspendedSales2 = 'suspended_sales_ch2',
+    receivedCards2 = 'received_cards_ch2',
+    scryfallBulkCards = 'scryfall_bulk_cards',
+    users = 'users',
+}
+
 interface Ch1Collection {
-    cardInventory: 'card_inventory';
-    salesData: 'sales_data';
-    suspendedSales: 'suspended_sales';
-    receivedCards: 'received_cards';
-    users: 'users';
+    cardInventory: Collection.cardInventory;
+    salesData: Collection.salesData;
+    suspendedSales: Collection.suspendedSales;
+    receivedCards: Collection.receivedCards;
+    users: Collection.users;
 }
 
 interface Ch2Collection {
-    cardInventory: 'card_inventory_ch2';
-    salesData: 'sales_data_ch2';
-    suspendedSales: 'suspended_sales_ch2';
-    receivedCards: 'received_cards_ch2';
-    users: 'users';
+    cardInventory: Collection.cardInventory2;
+    salesData: Collection.salesData2;
+    suspendedSales: Collection.suspendedSales2;
+    receivedCards: Collection.receivedCards2;
+    users: Collection.users;
 }
 
 export default function collectionFromLocation(
@@ -21,20 +34,20 @@ export default function collectionFromLocation(
 ): Ch1Collection | Ch2Collection {
     if (location === 'ch1') {
         return {
-            cardInventory: 'card_inventory',
-            salesData: 'sales_data',
-            suspendedSales: 'suspended_sales',
-            receivedCards: 'received_cards',
-            users: 'users',
+            cardInventory: Collection.cardInventory,
+            salesData: Collection.salesData,
+            suspendedSales: Collection.suspendedSales,
+            receivedCards: Collection.receivedCards,
+            users: Collection.users,
         };
     }
     if (location === 'ch2') {
         return {
-            cardInventory: 'card_inventory_ch2',
-            salesData: 'sales_data_ch2',
-            suspendedSales: 'suspended_sales_ch2',
-            receivedCards: 'received_cards_ch2',
-            users: 'users',
+            cardInventory: Collection.cardInventory2,
+            salesData: Collection.salesData2,
+            suspendedSales: Collection.suspendedSales2,
+            receivedCards: Collection.receivedCards2,
+            users: Collection.users,
         };
     }
 }

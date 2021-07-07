b1cdf9a2dbb7aaabff75e4c19947f80b504c9aa8
Admin <jaipkamat@gmail.com>
2021-07-06 23:51:04 -0400

    first pass of formik refactor of sale line items...needs testing and reset after sale finalization

--- frontend/src/NewSale/BrowseCardItem.tsx
+++ frontend/src/NewSale/BrowseCardItem.tsx
@@ -1,28 +1,17 @@
-import React, {
-    useState,
-    useContext,
-    FC,
-    SyntheticEvent,
-    ChangeEvent,
-} from 'react';
-import {
-    Segment,
-    Form,
-    Input,
-    Dropdown,
-    Button,
-    Item,
-} from 'semantic-ui-react';
+import React, { useContext, FC, ChangeEvent } from 'react';
+import { Segment, Form, Input, Button, Item } from 'semantic-ui-react';
 import $ from 'jquery';
 import _ from 'lodash';
 import CardImage from '../common/CardImage';
 import { SaleContext } from '../context/SaleContext';
 import { ScryfallCard, QOH } from '../utils/ScryfallCard';
 import CardHeader from '../ui/CardHeader';
+import { FormikErrors, useFormik } from 'formik';
+import FormikSelectField from '../ui/FormikSelectField';
 
 interface ConditionOptions {
     text: string;
-    value: string;
+    value: keyof QOH;
     key: string;
 }
 
@@ -43,7 +32,7 @@ function createConditionOptions(
 
         return {
             text: `${conditionFinish.split('_').join(' | ')} | Qty: ${qty}`,
-            value: conditionFinish,
+            value: conditionFinish as keyof QOH,
             key: `${id}${conditionFinish}`,
         };
     });
@@ -51,19 +40,10 @@ function createConditionOptions(
 
 type Finish = 'FOIL' | 'NONFOIL';
 
-/**
- * Creates initial selectedFinish value, used for the MarketPrice component
- * Returns FOIL or NONFOIL depending on what's in current inventory (qoh)
- * @param {Object} qoh
- */
-function createInitialSelectedFinish(qoh: Partial<QOH>): Finish {
-    const removeZeroedQuantites = _.pickBy(qoh, (p) => p && p > 0);
-    // Isolate only the FOIL or NONFOIL values with mapping
-    const keysMapped = _.keys(removeZeroedQuantites).map(
-        (k) => k.split('_')[0]
-    );
-    const uniqueValues = _.uniq(keysMapped);
-    return uniqueValues.indexOf('NONFOIL') > -1 ? 'NONFOIL' : 'FOIL';
+interface FormValues {
+    quantityToSell: number;
+    price: number;
+    selectedFinishCondition: keyof QOH;
 }
 
 interface Props {
@@ -71,92 +51,64 @@ interface Props {
 }
 
 const BrowseCardItem: FC<Props> = ({ card }) => {
-    const [selectedFinishCondition, setSelectedFinishCondition] = useState<
-        string
-    >('');
-    const [
-        selectedFinishConditionQty,
-        setSelectedFinishConditionQty,
-    ] = useState<number>(0);
-    const [quantityToSell, setQuantityToSell] = useState<number | null>(0);
-    const [price, setPrice] = useState<number | null>(0);
-    const [selectedFinish, setSelectedFinish] = useState<Finish>(
-        createInitialSelectedFinish(card.qoh)
-    );
-    const [conditionOptions, setConditionOptions] = useState<
-        ConditionOptions[]
-    >(createConditionOptions(card.qoh, card.id));
     const { addToSaleList } = useContext(SaleContext);
 
-    const handleQuantityChange = (
-        e: SyntheticEvent,
-        { value }: { value: string }
-    ) => {
-        if (value === '') {
-            setQuantityToSell(null);
-            return;
-        }
+    const conditionSelectOptions = createConditionOptions(card.qoh, card.id);
 
-        let numVal = parseInt(value);
+    const handleAddToSale = ({
+        selectedFinishCondition,
+        quantityToSell,
+        price,
+    }: FormValues) => {
+        addToSaleList(card, selectedFinishCondition, quantityToSell, price);
 
-        if (numVal > selectedFinishConditionQty)
-            numVal = selectedFinishConditionQty;
-
-        if (isNaN(numVal) || numVal < 0) numVal = 0;
-
-        setQuantityToSell(numVal);
-    };
-
-    const handleSelectedFinishCondition = (
-        e: SyntheticEvent,
-        { value }: { value: keyof QOH }
-    ) => {
-        // TODO: we need to not coerce here
-        setSelectedFinish(value.split('_')[0] as Finish);
-        setSelectedFinishCondition(value);
-        setSelectedFinishConditionQty(card.qoh[value] || 0);
-        setQuantityToSell(0);
-    };
-
-    const handlePriceChange = (
-        e: SyntheticEvent,
-        { value }: { value: string }
-    ) => {
-        if (value === '') {
-            setPrice(null);
-            return;
-        }
-
-        let numVal = Number(value);
-
-        if (isNaN(numVal) || numVal < 0) {
-            numVal = 0;
-        }
-
-        setPrice(numVal);
-    };
-
-    const handleAddToSale = () => {
-        const { id } = card;
-
-        addToSaleList(
-            card,
-            selectedFinishCondition,
-            quantityToSell || 0,
-            price || 0
-        );
-
-        // Reset state
-        setSelectedFinishCondition('');
-        setSelectedFinishConditionQty(0);
-        setQuantityToSell(0);
-        setPrice(0);
-        setConditionOptions(createConditionOptions(card.qoh, id));
-        setSelectedFinish(createInitialSelectedFinish(card.qoh));
+        // TODO: Clear whole sale context state when a sate completes
 
         // Highlight the input after successful card add
         $('#searchBar').focus().select();
     };
+
+    const initialFormValues = {
+        selectedFinishCondition: conditionSelectOptions[0].value,
+        price: 0,
+        quantityToSell: 0,
+    };
+
+    const validate = ({
+        quantityToSell,
+        price,
+        selectedFinishCondition: selectedFinish,
+    }: FormValues) => {
+        const errors: FormikErrors<FormValues> = {};
+
+        if (!quantityToSell) errors.quantityToSell = 'error';
+        if (!price) errors.price = 'error';
+
+        if (!selectedFinish) {
+            errors.selectedFinishCondition = 'error';
+        }
+
+        if (quantityToSell > card.qoh[selectedFinish]!) {
+            errors.quantityToSell = 'error';
+        }
+
+        if (price < 0) {
+            errors.price = 'error';
+        }
+
+        if (quantityToSell < 1) {
+            errors.quantityToSell = 'error';
+        }
+
+        return errors;
+    };
+
+    const { handleSubmit, setFieldValue, values, isValid } = useFormik({
+        initialValues: initialFormValues,
+        validate,
+        onSubmit: handleAddToSale,
+        validateOnMount: true,
+    });
 
     return (
         <Segment>
@@ -168,58 +120,86 @@ const BrowseCardItem: FC<Props> = ({ card }) => {
                     <Item.Content>
                         <CardHeader
                             card={card}
-                            selectedFinish={selectedFinish}
+                            selectedFinish={
+                                values.selectedFinishCondition.split(
+                                    '_'
+                                )[0] as Finish
+                            }
                             showMid
                             round
                         />
                         <Item.Description>
+                            <pre>{JSON.stringify({ values }, null, 2)}</pre>
                             <Form>
                                 <Form.Group>
-                                    <Form.Field
-                                        className="finish-condition"
-                                        control={Dropdown}
-                                        selection
-                                        placeholder="Select inventory"
-                                        options={conditionOptions}
-                                        value={selectedFinishCondition}
+                                    <FormikSelectField
                                         label="Select finish/condition"
-                                        onChange={handleSelectedFinishCondition}
+                                        name="selectedFinishCondition"
+                                        options={conditionSelectOptions}
+                                        defaultValue={
+                                            initialFormValues.selectedFinishCondition
+                                        }
+                                        onChange={(v) => {
+                                            setFieldValue(
+                                                'selectedFinishCondition',
+                                                v
+                                            );
+                                            setFieldValue('quantityToSell', 0);
+                                        }}
                                     />
                                     <Form.Field
-                                        className="sale-qty"
                                         control={Input}
                                         type="number"
                                         label="Quantity to sell"
-                                        value={quantityToSell}
-                                        onChange={handleQuantityChange}
-                                        disabled={!selectedFinishConditionQty}
+                                        value={values.quantityToSell}
+                                        onChange={(
+                                            _: any,
+                                            { value }: { value: string }
+                                        ) => {
+                                            const castVal = parseInt(value, 10);
+
+                                            if (
+                                                castVal >
+                                                card.qoh[
+                                                    values
+                                                        .selectedFinishCondition
+                                                ]!
+                                            ) {
+                                                return;
+                                            }
+
+                                            setFieldValue(
+                                                'quantityToSell',
+                                                castVal
+                                            );
+                                        }}
                                         onFocus={(
                                             e: ChangeEvent<HTMLInputElement>
                                         ) => e.target.select()}
                                     />
                                     <Form.Field
-                                        className="sale-price"
                                         control={Input}
                                         type="number"
                                         label="Price"
-                                        value={price}
-                                        onChange={handlePriceChange}
-                                        disabled={!selectedFinishConditionQty}
+                                        value={values.price}
+                                        onChange={(
+                                            _: any,
+                                            { value }: { value: string }
+                                        ) => {
+                                            const castVal = parseFloat(value);
+                                            setFieldValue('price', castVal);
+                                        }}
                                         onFocus={(
                                             e: ChangeEvent<HTMLInputElement>
                                         ) => e.target.select()}
                                         step={0.5}
                                     />
                                     <Form.Button
-                                        className="add-to-sale"
                                         label="Add to sale?"
                                         control={Button}
                                         primary
-                                        onClick={handleAddToSale}
-                                        disabled={
-                                            !quantityToSell ||
-                                            quantityToSell <= 0
-                                        }
+                                        onClick={() => handleSubmit()}
+                                        disabled={!isValid}
                                     >
                                         Sell
                                     </Form.Button>

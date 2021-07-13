15454a25f426428c4d45ac24306142d19ce73e91
Admin <jaipkamat@gmail.com>
2021-07-13 15:47:50 -0700

    created controlled search bar, implemented it in manage inventory

--- frontend/src/ManageInventory/ManageInventory.tsx
+++ frontend/src/ManageInventory/ManageInventory.tsx
@@ -1,18 +1,31 @@
-import React, { useContext } from 'react';
-import SearchBar from '../common/SearchBar';
+import React, { useContext, useEffect, useState } from 'react';
 import { Segment, Header, Icon, Divider } from 'semantic-ui-react';
 import AllLocationInventory from './AllLocationInventory';
 import { InventoryContext } from '../context/InventoryContext';
 import ManageInventoryListItem from './ManageInventoryListItem';
 import { Grid } from '@material-ui/core';
 import { HeaderText } from '../ui/Typography';
+import ControlledSearchBar from '../common/ControlledSearchBar';
+import Loading from '../ui/Loading';
 
 export default function ManageInventory() {
+    const [term, setTerm] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
     const { searchResults, handleSearchSelect } = useContext(InventoryContext);
+
+    useEffect(() => {
+        if (term) {
+            (async () => {
+                setLoading(true);
+                await handleSearchSelect(term);
+                setLoading(false);
+            })();
+        }
+    }, [term]);
 
     return (
         <>
-            <SearchBar handleSearchSelect={handleSearchSelect} />
+            <ControlledSearchBar value={term} onChange={(v) => setTerm(v)} />
             <br />
             <Grid container justify="space-between">
                 <HeaderText>Manage Inventory</HeaderText>
@@ -24,24 +37,30 @@ export default function ManageInventory() {
                 )}
             </Grid>
             <Divider />
-            {!searchResults.length && (
-                <Segment placeholder>
-                    <Header icon>
-                        <Icon name="search" />
-                        <em>
-                            "For the first time in his life, Grakk felt a little
-                            warm and fuzzy inside."
-                        </em>
-                    </Header>
-                </Segment>
-            )}
-            <Grid container spacing={2}>
-                {searchResults.map((card) => (
-                    <Grid item xs={12} key={card.id}>
-                        <ManageInventoryListItem card={card} />
+            {loading ? (
+                <Loading />
+            ) : (
+                <>
+                    {!searchResults.length && (
+                        <Segment placeholder>
+                            <Header icon>
+                                <Icon name="search" />
+                                <em>
+                                    "For the first time in his life, Grakk felt
+                                    a little warm and fuzzy inside."
+                                </em>
+                            </Header>
+                        </Segment>
+                    )}
+                    <Grid container spacing={2}>
+                        {searchResults.map((card) => (
+                            <Grid item xs={12} key={card.id}>
+                                <ManageInventoryListItem card={card} />
+                            </Grid>
+                        ))}
                     </Grid>
-                ))}
-            </Grid>
+                </>
+            )}
         </>
     );
 }

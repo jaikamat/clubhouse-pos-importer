e67ad2cf55bedd6880be8d0c3ec188fe8ffb1e2d
Admin <jaipkamat@gmail.com>
2021-07-14 10:40:29 -0700

    controlled search in browse sales

--- frontend/src/BrowseSales/BrowseSales.tsx
+++ frontend/src/BrowseSales/BrowseSales.tsx
@@ -1,11 +1,13 @@
-import React, { FC, useState } from 'react';
-import SearchBar from '../common/SearchBar';
+import React, { FC, useEffect, useState } from 'react';
 import SalesList from './SalesList';
 import { Divider } from 'semantic-ui-react';
 import browseSalesQuery, { Sale } from './browseSalesQuery';
 import { HeaderText } from '../ui/Typography';
+import ControlledSearchBar from '../common/ControlledSearchBar';
 
 const BrowseSales: FC = () => {
+    const [term, setTerm] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
     const [salesList, setSalesList] = useState<Sale[]>([]);
     const [cardName, setCardName] = useState<string>('');
 
@@ -16,9 +18,19 @@ const BrowseSales: FC = () => {
         setCardName(cardName);
     };
 
+    useEffect(() => {
+        if (term) {
+            (async () => {
+                setLoading(true);
+                await handleSearchSelect(term);
+                setLoading(false);
+            })();
+        }
+    }, [term]);
+
     return (
         <div>
-            <SearchBar handleSearchSelect={handleSearchSelect} />
+            <ControlledSearchBar value={term} onChange={(v) => setTerm(v)} />
             <br />
             <HeaderText>Browse Sales</HeaderText>
             <Divider />

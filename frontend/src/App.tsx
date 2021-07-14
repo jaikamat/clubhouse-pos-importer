ec68105faaba7f430607d707397b2592e86fdb95
Admin <jaipkamat@gmail.com>
2021-07-14 15:20:14 -0700

    remove development scaffolding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import React, { FC, useState } from 'react';
-import Header from './Header/Header';
+import React, { FC } from 'react';
 import AuthProvider from './context/AuthProvider';
 import { Switch, Route } from 'react-router-dom';
 import ManageInventory from './ManageInventory/ManageInventory';
@@ -10,26 +9,26 @@ import DeckboxClone from './DeckboxClone/DeckboxClone';
 import Login from './Login/Login';
 import Logout from './Logout/Logout';
 import Receiving from './Receiving/Receiving';
-import styled from 'styled-components';
 import { SaleProvider } from './context/SaleContext';
 import ReceivingProvider from './context/ReceivingContext';
 import InventoryProvider from './context/InventoryContext';
 import AdminRoute from './AuthenticatedRoute';
 import Home from './LandingPage/Home';
 import BrowseReceiving from './BrowseReceiving/BrowseReceiving';
-import { Box, createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core';
 import NavBar from './Header/NavBar';
 
-const ContentContainer = styled.div`
-    padding-top: 75px;
-    margin-left: 20px;
-    margin-right: 20px;
-`;
-
-const BackgroundColor = styled.div`
-    background-color: #f9fafb;
-    min-height: 100vh;
-`;
+const useStyles = makeStyles(({ spacing }) => ({
+    contentContainer: {
+        paddingTop: spacing(10),
+        marginLeft: spacing(3),
+        marginRight: spacing(3),
+    },
+    backgroundColor: {
+        backgroundColor: '#f9fafb',
+        minHeight: '100vh',
+    },
+}));
 
 const theme = createMuiTheme({
     palette: {
@@ -40,20 +39,16 @@ const theme = createMuiTheme({
 });
 
 const App: FC = () => {
-    const [showBar, setShowBar] = useState<boolean>(false);
+    const { backgroundColor, contentContainer } = useStyles();
 
     return (
         <AuthProvider>
             <ThemeProvider theme={theme}>
-                <Header />
-                {showBar && <NavBar />}
-                <Box pt={10}>
-                    <button onClick={() => setShowBar(!showBar)}>Switch</button>
-                </Box>
+                <NavBar />
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <BackgroundColor>
-                        <ContentContainer id="content-container">
+                    <div className={backgroundColor}>
+                        <div className={contentContainer}>
                             <AdminRoute exact path="/manage-inventory">
                                 <InventoryProvider>
                                     <ManageInventory />
@@ -85,8 +80,8 @@ const App: FC = () => {
                             />
                             <Route exact path="/login" component={Login} />
                             <Route exact path="/logout" component={Logout} />
-                        </ContentContainer>
-                    </BackgroundColor>
+                        </div>
+                    </div>
                 </Switch>
             </ThemeProvider>
         </AuthProvider>

d66bccc258d2281d59505480d0d8833fbaec186f
Jai Kamat <jaipkamat@gmail.com>
2021-08-04 12:46:24 -0700

    streamline InventoryChip styling

--- frontend/src/ui/InventoryChip.tsx
+++ frontend/src/ui/InventoryChip.tsx
@@ -2,19 +2,27 @@ import React, { FC } from 'react';
 import { Chip, makeStyles } from '@material-ui/core';
 import { forwardRef } from 'react';
 
-const useStyles = makeStyles(({ palette }) => ({
+const useStyles = makeStyles({
     container: {
-        backgroundColor: ({ color }: { color?: 'primary' }) => {
-            if (color === 'primary') return palette.primary.main;
-            else return palette.grey[300];
-        },
         display: 'inline-block',
         borderRadius: 5,
     },
     chip: {
         borderRadius: 5,
     },
-}));
+    leftChip: {
+        borderTopRightRadius: 0,
+        borderBottomRightRadius: 0,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5,
+    },
+    rightChip: {
+        borderTopLeftRadius: 0,
+        borderBottomLeftRadius: 0,
+        borderTopRightRadius: 5,
+        borderBottomRightRadius: 5,
+    },
+});
 
 interface Props {
     label: string;
@@ -28,23 +36,20 @@ interface Props {
 const InventoryChip: FC<Props> = forwardRef<HTMLDivElement, Props>(
     (props, ref) => {
         const { quantity, label } = props;
+        const { container, leftChip, rightChip } = useStyles();
         const quantityColor = quantity > 0 ? 'primary' : undefined;
-
-        const { container, chip } = useStyles({
-            color: quantityColor,
-        });
 
         return (
             <div {...props} ref={ref} className={container}>
                 <Chip
                     color={quantityColor}
-                    className={chip}
+                    className={leftChip}
                     size="small"
                     label={label}
                 />
                 <Chip
                     color={quantityColor}
-                    className={chip}
+                    className={rightChip}
                     size="small"
                     label={quantity}
                 />

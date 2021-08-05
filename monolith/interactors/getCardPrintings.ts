e8a739a033f7b613605e92be48d460bb9dce7c15
Jai Kamat <jaipkamat@gmail.com>
2021-08-05 14:03:33 -0700

    create friendly detailed display name and match only english cards

--- monolith/interactors/getCardPrintings.ts
+++ monolith/interactors/getCardPrintings.ts
@@ -2,33 +2,66 @@ import { Collection } from '../common/types';
 import getDatabaseConnection from '../database';
 import ScryfallCard from './ScryfallCard';
 
-// etched: boolean
-// found in frame effects
-// borderless: boolean
-// found in border_color
-// showcase: boolean
-// found in frame effects
+/**
+ * Creates a detailed user-friendly name for cards to discern what they are, without imagery
+ */
+function cardDisplayName(card: ScryfallCard): string {
+    const treatments: string[] = [];
+    const baseTitle = card.name;
+    const setName = card.set_name;
+
+    const isPrereleasePromo =
+        card.promo_types && card.promo_types.includes('prerelease');
+
+    const isPromoPackPromo =
+        card.promo_types && card.promo_types.includes('promopack');
+
+    const isStampedPromo =
+        card.promo_types && card.promo_types.includes('stamped');
+
+    const isShowcase =
+        card.frame_effects && card.frame_effects.includes('showcase');
+
+    const isBorderless = card.border_color === 'borderless';
+
+    const isEtched =
+        card.frame_effects && card.frame_effects.includes('etched');
+
+    const isExtendedArt =
+        card.frame_effects && card.frame_effects.includes('extendedart');
+
+    if (isPrereleasePromo) treatments.push('Prerelease promo');
+    if (isPromoPackPromo) treatments.push('Promo pack');
+    if (isStampedPromo) treatments.push('Stamped');
+    if (isShowcase) treatments.push('Showcase');
+    if (isBorderless) treatments.push('Borderless');
+    if (isEtched) treatments.push('Etched');
+    if (isExtendedArt) treatments.push('Extended art');
+
+    const separator = treatments.length > 0 ? ' - ' : '';
+
+    return `${baseTitle} - ${setName}${separator}${treatments.join(', ')}`;
+}
+
 class BulkCard {
     public scryfall_id: string;
     public name: string;
+    public display_name: string;
     public set_abbreviation: string;
     public set_name: string;
     public rarity: string;
-    public frame_effects: string[] | null;
-    public foil: boolean;
-    public nonfoil: boolean;
-    public border_color: string;
+    public foil_printing: boolean;
+    public nonfoil_printing: boolean;
 
     constructor(card: ScryfallCard) {
         this.scryfall_id = card.id;
         this.name = card.name;
+        this.display_name = cardDisplayName(card);
         this.set_abbreviation = card.set;
         this.set_name = card.set_name;
         this.rarity = card.rarity;
-        this.frame_effects = card.frame_effects;
-        this.foil = card.foil;
-        this.nonfoil = card.nonfoil;
-        this.border_color = card.border_color;
+        this.foil_printing = card.foil;
+        this.nonfoil_printing = card.nonfoil;
     }
 }
 
@@ -37,6 +70,11 @@ async function getCardPrintings(cardName: string) {
         const db = await getDatabaseConnection();
 
         const pipeline = [];
+
+        // Match only english cards
+        const match = {
+            lang: 'en',
+        };
 
         // Text match on card title
         const search = {
@@ -49,6 +87,7 @@ async function getCardPrintings(cardName: string) {
         };
 
         pipeline.push({ $search: search });
+        pipeline.push({ $match: match });
 
         const cards = await db
             .collection(Collection.scryfallBulkCards)

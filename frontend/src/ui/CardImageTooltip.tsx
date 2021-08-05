97eb236c622d9d39b0325ba5c840674aa5000a45
Jai Kamat <jaipkamat@gmail.com>
2021-08-05 11:26:19 -0700

    use MUI and clsc for CardImageTooltip

--- frontend/src/ui/CardImageTooltip.tsx
+++ frontend/src/ui/CardImageTooltip.tsx
@@ -1,6 +1,6 @@
 import { makeStyles, Tooltip } from '@material-ui/core';
 import { FC } from 'react';
-import { Image } from 'semantic-ui-react';
+import clsx from 'clsx';
 
 interface Props {
     cardImage: string;
@@ -13,16 +13,23 @@ const useStyles = makeStyles({
     borderRounded: {
         borderRadius: '7px 7px 7px 7px',
     },
+    imageSize: {
+        width: 155,
+        height: 'auto',
+    },
 });
 
 const CardImageTooltip: FC<Props> = ({ cardImage, children }) => {
-    const { transparentBackground, borderRounded } = useStyles();
+    const { transparentBackground, borderRounded, imageSize } = useStyles();
 
     return (
         <Tooltip
             placement="bottom-start"
             title={
-                <Image className={borderRounded} size="small" src={cardImage} />
+                <img
+                    className={clsx(borderRounded, imageSize)}
+                    src={cardImage}
+                />
             }
             classes={{
                 tooltip: transparentBackground,

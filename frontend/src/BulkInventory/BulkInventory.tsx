1820eefd29799abfbc66bb2f625436335da26b2b
Jai Kamat <jaipkamat@gmail.com>
2021-08-06 11:56:11 -0700

    support foiling changes through formik

--- frontend/src/BulkInventory/BulkInventory.tsx
+++ frontend/src/BulkInventory/BulkInventory.tsx
@@ -1,30 +1,117 @@
-import { Container, Grid } from '@material-ui/core';
+import {
+    Container,
+    FormControl,
+    Grid,
+    InputLabel,
+    MenuItem,
+    Select,
+} from '@material-ui/core';
+import { useFormik } from 'formik';
 import React, { FC } from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import CardImage from '../common/CardImage';
 import { BulkCard } from './bulkInventoryQuery';
 import BulkSearchBar from './BulkSearchBar';
 
+type Finish = 'FOIL' | 'NONFOIL';
+type Condition = 'NM' | 'LP' | 'MP' | 'HP';
+
+interface FormValues {
+    bulkCard: BulkCard | null;
+    finish: Finish;
+    quanitity: number;
+    condition: Condition;
+}
+
 const BulkInventory: FC = () => {
-    const [bulkCard, setBulkCard] = useState<BulkCard | null>(null);
     const [currentCardImage, setCurrentCardImage] = useState<string>('');
+
+    const { values, setFieldValue } = useFormik<FormValues>({
+        initialValues: {
+            bulkCard: null,
+            finish: 'NONFOIL',
+            quanitity: 1,
+            condition: 'NM',
+        },
+        onSubmit: async (v: FormValues) => {
+            alert(JSON.stringify(v, null, 2));
+        },
+    });
+
+    useEffect(() => {
+        if (values.bulkCard) {
+            // If _only_ a foil printing exists, set to foil
+            if (!values.bulkCard.nonfoil_printing) {
+                setFieldValue('finish', 'FOIL');
+                return;
+            }
+            // If _only_ a nonfoil printing exists, set to nonfoil
+            if (!values.bulkCard.foil_printing) {
+                setFieldValue('finish', 'NONFOIL');
+                return;
+            }
+            // Otherwise both exist, default to nonfoil
+            setFieldValue('finish', 'NONFOIL');
+        }
+    }, [values]);
 
     return (
         <Container>
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <BulkSearchBar
-                        value={bulkCard}
-                        onChange={(v) => setBulkCard(v)}
-                        onHighlight={(o) => setCurrentCardImage(o?.image || '')}
-                    />
+            <pre>{JSON.stringify({ values }, null, 2)}</pre>
+            <form>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <BulkSearchBar
+                            value={values.bulkCard}
+                            onChange={(v) => setFieldValue('bulkCard', v)}
+                            onHighlight={(o) =>
+                                setCurrentCardImage(o?.image || '')
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <FormControl
+                            variant="outlined"
+                            size="small"
+                            fullWidth
+                            disabled={!values.bulkCard}
+                        >
+                            <InputLabel>Finish</InputLabel>
+                            <Select
+                                label="Finish"
+                                value={values.finish}
+                                onChange={(e) =>
+                                    setFieldValue(
+                                        'finish',
+                                        e.target.value as string
+                                    )
+                                }
+                            >
+                                <MenuItem
+                                    key="nonfoil"
+                                    value="FOIL"
+                                    disabled={!values?.bulkCard?.foil_printing}
+                                >
+                                    Foil
+                                </MenuItem>
+                                <MenuItem
+                                    key="foil"
+                                    value="NONFOIL"
+                                    disabled={
+                                        !values?.bulkCard?.nonfoil_printing
+                                    }
+                                >
+                                    Nonfoil
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
+                        <div>
+                            <CardImage image={currentCardImage} />
+                        </div>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6}>
-                    <div>
-                        <CardImage image={currentCardImage} />
-                    </div>
-                </Grid>
-            </Grid>
+            </form>
         </Container>
     );
 };

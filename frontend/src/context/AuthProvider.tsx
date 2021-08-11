6bd41b45b681a5eec5c1369e5a43d966e2dc1d87
Jai Kamat <jaipkamat@gmail.com>
2021-08-11 12:41:37 -0700

    add multi-tab implementation to auth provider and extract localStorage keys

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -1,10 +1,14 @@
-import React, { FC, useContext, useState } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import loginQuery from './loginQuery';
 
 interface Props {}
 
 export type ClubhouseLocation = 'ch1' | 'ch2';
+
+const tokenKey = 'clubhouse_JWT';
+const locationKey = 'currentLocation';
+const userKey = 'currentUser';
 
 interface Context {
     loggedIn: boolean;
@@ -30,20 +34,36 @@ export const useAuthContext = () => useContext(AuthContext);
 
 const AuthProvider: FC<Props> = ({ children }) => {
     const history = useHistory();
-    const [loggedIn, setLoggedIn] = useState(
-        !!localStorage.getItem('clubhouse_JWT')
-    );
+    const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem(tokenKey));
 
     const [
         currentLocation,
         setCurrentLocation,
     ] = useState<ClubhouseLocation | null>(
-        localStorage.getItem('currentLocation') as ClubhouseLocation
+        localStorage.getItem(locationKey) as ClubhouseLocation
     );
 
     const [currentUser, setCurrentUser] = useState<string | null>(
-        localStorage.getItem('currentUser')
+        localStorage.getItem(userKey)
     );
+
+    /**
+     * Handles multi-tab logouts.
+     *
+     * If the token is cleared and the new value is `null`,
+     * we issue a logout to all other tabs
+     */
+    useEffect(() => {
+        const storageListener = (e: StorageEvent) => {
+            if (e.key === tokenKey && e.newValue === null) {
+                handleLogout();
+            }
+        };
+
+        window.addEventListener('storage', storageListener);
+
+        return () => window.removeEventListener('storage', storageListener);
+    }, []);
 
     const handleLogin = async (
         username: string,
@@ -54,13 +74,13 @@ const AuthProvider: FC<Props> = ({ children }) => {
             const data = await loginQuery(username, password, currentLocation);
 
             if (data.token) {
-                localStorage.setItem('clubhouse_JWT', data.token);
-                setLoggedIn(!!localStorage.getItem('clubhouse_JWT'));
+                localStorage.setItem(tokenKey, data.token);
+                setLoggedIn(!!localStorage.getItem(tokenKey));
 
-                localStorage.setItem('currentLocation', currentLocation);
+                localStorage.setItem(locationKey, currentLocation);
                 setCurrentLocation(currentLocation);
 
-                localStorage.setItem('currentUser', username);
+                localStorage.setItem(userKey, username);
                 setCurrentUser(username);
             }
 
@@ -71,13 +91,13 @@ const AuthProvider: FC<Props> = ({ children }) => {
     };
 
     const handleLogout = () => {
-        localStorage.removeItem('clubhouse_JWT');
-        setLoggedIn(!!localStorage.getItem('clubhouse_JWT'));
+        localStorage.removeItem(tokenKey);
+        setLoggedIn(!!localStorage.getItem(tokenKey));
 
-        localStorage.removeItem('currentLocation');
+        localStorage.removeItem(locationKey);
         setCurrentLocation(null);
 
-        localStorage.removeItem('currentUser');
+        localStorage.removeItem(userKey);
         setCurrentUser(null);
 
         history.push('/login');

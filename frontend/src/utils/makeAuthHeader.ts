2678bd00e2c7f4f32d95ca57db069a1327b2abae
Jai Kamat <jaipkamat@gmail.com>
2021-08-11 13:59:05 -0700

    reconstruct auth header from token using parse

--- frontend/src/utils/makeAuthHeader.ts
+++ frontend/src/utils/makeAuthHeader.ts
@@ -2,8 +2,9 @@
  * This function is a helper for Axios, it yields an object for Axios params
  */
 export default function makeAuthHeader() {
-    const header = {
-        Authorization: `Bearer ${localStorage.getItem('clubhouse_JWT')}`,
-    };
-    return header;
+    const item = localStorage.getItem('clubhouse_JWT');
+
+    if (!item) return null;
+
+    return { Authorization: `Bearer ${JSON.parse(item)}` };
 }

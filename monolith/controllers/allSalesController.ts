13ab4f798689a0e3ba7f8868737f54a2f0e31920
Jai Kamat <jaipkamat@gmail.com>
2021-08-13 12:48:31 -0700

    add correct response type in getAllSales

--- monolith/controllers/allSalesController.ts
+++ monolith/controllers/allSalesController.ts
@@ -2,8 +2,9 @@ require('dotenv').config();
 import getAllSales from '../interactors/getAllSales';
 import getFormatLegalities from '../interactors/getFormatLegalities';
 import { RequestWithUserInfo } from '../common/types';
+import { Response } from 'express';
 
-const allSalesController = async (req: RequestWithUserInfo, res) => {
+const allSalesController = async (req: RequestWithUserInfo, res: Response) => {
     try {
         const sales_data = await getAllSales(req.currentLocation);
         const format_legalities = await getFormatLegalities(

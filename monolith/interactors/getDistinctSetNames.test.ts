fddba9bbb90ddd752f259fdb04f7a4fa2f688b05
Jai Kamat <jaipkamat@gmail.com>
2021-09-16 14:54:25 -0700

    testing for getDistinctSetNames

---
+++ monolith/interactors/getDistinctSetNames.test.ts
@@ -0,0 +1,64 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import getDatabaseConnection from '../database';
+import addCardToInventory from './addCardToInventory';
+import getDistinctSetNames from './getDistinctSetNames';
+
+const SCRYFALL_BULK = 'scryfall_bulk_cards';
+
+let mongoServer;
+let db;
+
+// Set up the mongo memory instance
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    db = await getDatabaseConnection(uri);
+
+    // Add some cards to store inventory collection with differing set namees
+    const inserts = [
+        'foo',
+        'bar',
+        'baz',
+        'bin',
+        'repeat',
+        'woot',
+        'nub',
+        'repeat',
+    ].map((el) => {
+        return addCardToInventory({
+            quantity: 4,
+            finishCondition: 'FOIL_NM',
+            id: Math.random().toString(),
+            name: el,
+            set_name: el,
+            set: el,
+            location: 'ch1',
+        });
+    });
+
+    await Promise.all(inserts);
+});
+
+afterAll(async () => {
+    await mongoServer.stop();
+});
+
+test('Get all the unique set names', async () => {
+    const ch1Names = await getDistinctSetNames('ch1');
+    const ch2Names = await getDistinctSetNames('ch2');
+
+    expect({ ch1Names, ch2Names }).toMatchInlineSnapshot(`
+Object {
+  "ch1Names": Array [
+    "bar",
+    "baz",
+    "bin",
+    "foo",
+    "nub",
+    "repeat",
+    "woot",
+  ],
+  "ch2Names": Array [],
+}
+`);
+});

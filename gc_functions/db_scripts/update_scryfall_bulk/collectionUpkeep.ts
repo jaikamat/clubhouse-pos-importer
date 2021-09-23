ae73d89f07d98714085051fd667c774763edc49f
Jai Kamat <jaipkamat@gmail.com>
2021-09-22 17:12:29 -0700

    create new function to delete all collection documents and recreate indexes

---
+++ gc_functions/db_scripts/update_scryfall_bulk/collectionUpkeep.ts
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const COLLECTION = "scryfall_bulk_cards";
+
+async function collectionUpkeep(databaseName: string) {
+    const mongoSettings = { useNewUrlParser: true, useUnifiedTopology: true };
+
+    try {
+        const { MONGO_URI } = process.env;
+        var client = await MongoClient.connect(MONGO_URI, mongoSettings); // 'const' scopes to the block, we need access in 'finally' to close the connection
+        const db = await client.db(databaseName || "test");
+
+        console.log(`Connected to MongoDB database: ${db.databaseName}`);
+
+        // Recreate desired collection indexes
+        console.log("Recreating collection indexes...");
+        await db
+            .collection(COLLECTION)
+            .createIndexes([
+                { key: { id: 1 } },
+                { key: { name: 1, games: 1 } },
+                { key: { name: 1 } },
+            ]);
+        console.log("Collection indexes recreated");
+
+        // Delete all elements in the collection
+        console.log("Deleting collection documents...");
+        await db.collection(COLLECTION).deleteMany({});
+        console.log("Documents deleted, preparing to insert fresh cards");
+    } catch (error) {
+        throw error;
+    } finally {
+        await client.close();
+        console.log("Disconnected from MongoDB");
+    }
+}
+
+export default collectionUpkeep;

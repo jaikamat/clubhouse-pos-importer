24eb267a00fc91bc4d7aa68f58c48fd3352a09d1
Jai Kamat <jaipkamat@gmail.com>
2021-10-01 15:35:43 -0700

    include seed data in tests for realism

--- monolith/interactors/deleteSuspendedSale.test.ts
+++ monolith/interactors/deleteSuspendedSale.test.ts
@@ -1,6 +1,7 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { FinishSaleCard } from '../common/types';
 import getDatabaseConnection from '../database';
+import timeSpiral from '../fixtures/fixtures';
 import collectionFromLocation from '../lib/collectionFromLocation';
 import addCardToInventory from './addCardToInventory';
 import createSuspendedSale from './createSuspendedSale';
@@ -9,6 +10,7 @@ import getSuspendedSales from './getSuspendedSales';
 
 let mongoServer;
 let db;
+const bulkId = timeSpiral.id;
 
 // Set up the mongo memory instance
 beforeAll(async () => {
@@ -16,14 +18,27 @@ beforeAll(async () => {
     const uri = mongoServer.getUri();
     db = await getDatabaseConnection(uri);
 
+    // Seed bulk data
+    await db.collection('scryfall_bulk_cards').bulkWrite([
+        {
+            updateOne: {
+                filter: { _id: bulkId }, // upsert the inserted cards to have a String _id rather than ObjectId
+                update: {
+                    $set: { ...timeSpiral },
+                },
+                upsert: true,
+            },
+        },
+    ]);
+
     const saleListCards: FinishSaleCard[] = [
         {
-            id: '4567',
+            id: bulkId,
             price: 2.34,
             qtyToSell: 2,
             finishCondition: 'NONFOIL_NM',
-            name: 'Card Name',
-            set_name: 'SNM',
+            name: timeSpiral.name,
+            set_name: timeSpiral.set_name,
         },
     ];
 
@@ -31,10 +46,10 @@ beforeAll(async () => {
     await addCardToInventory({
         quantity: 4,
         finishCondition: 'NONFOIL_NM',
-        id: '4567',
-        name: 'Card Name',
-        set_name: 'Set Name',
-        set: 'SNM',
+        id: bulkId,
+        name: timeSpiral.name,
+        set_name: timeSpiral.set_name,
+        set: timeSpiral.set,
         location: 'ch1',
     });
 
@@ -59,7 +74,7 @@ test('Deleting suspended sale restores inventory', async () => {
 
     const beforeSuspendDoc = await db
         .collection(collectionFromLocation('ch1').cardInventory)
-        .findOne({ _id: '4567' });
+        .findOne({ _id: bulkId });
 
     expect(beforeSuspendDoc.qoh.NONFOIL_NM).toBe(2);
 
@@ -67,7 +82,7 @@ test('Deleting suspended sale restores inventory', async () => {
 
     const afterSuspendDoc = await db
         .collection(collectionFromLocation('ch1').cardInventory)
-        .findOne({ _id: '4567' });
+        .findOne({ _id: bulkId });
 
     expect(afterSuspendDoc.qoh.NONFOIL_NM).toBe(4);
 });

12135bad1d1b8d2df9c2958ae1c93f2adacfe128
Jai Kamat <jaipkamat@gmail.com>
2021-10-04 17:51:38 -0700

    create error wrapper for controllers

---
+++ monolith/controllers/errorBoundary.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+
+type Controller = (req: Request, res: Response, next: NextFunction) => void;
+
+const errorBoundary = (controllerFunction: Controller) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await controllerFunction(req, res, next);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(error);
+        }
+    };
+};
+
+export default errorBoundary;

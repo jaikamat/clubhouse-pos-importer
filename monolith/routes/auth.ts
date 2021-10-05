70c35fc9b725691dc1e1ef93030c2260d4e10cd3
Jai Kamat <jaipkamat@gmail.com>
2021-10-04 17:53:25 -0700

    wrap all controllers in error wrapper

--- monolith/routes/auth.ts
+++ monolith/routes/auth.ts
@@ -20,25 +20,29 @@ import {
     salesReportController,
     suspendedSaleByIdController,
 } from '../controllers';
+import errorBoundary from '../controllers/errorBoundary';
 const router = express.Router();
 
 router.use(authController);
-router.post('/addCardToInventory', addCardToInventoryController);
-router.post('/finishSale', finishSaleValidationController);
-router.post('/finishSale', finishSaleController);
-router.get('/getSaleByTitle', getSalesByTitleController);
-router.post('/receiveCards', receiveCardsValidationController);
-router.post('/receiveCards', receiveCardsController);
-router.get('/suspendSale', getSuspendedSalesController);
-router.get('/suspendSale/:id', suspendedSaleByIdController);
-router.post('/suspendSale', createSuspendedSaleController);
-router.delete('/suspendSale/:id', deleteSuspendedSaleController);
-router.get('/getCardsByFilter', getCardsByFilterController);
-router.get('/getDistinctSetNames', distinctSetNamesController);
-router.get('/getCardsWithInfo', getCardsWithInfoController);
-router.get('/getReceivedCards', getReceivedCardsController);
-router.get('/getReceivedCards/:id', getReceivedCardsByIdController);
-router.get('/getSalesReport', salesReportController);
-router.get('/bulkSearch', bulkSearchController);
+router.post('/addCardToInventory', errorBoundary(addCardToInventoryController));
+router.post('/finishSale', errorBoundary(finishSaleValidationController));
+router.post('/finishSale', errorBoundary(finishSaleController));
+router.get('/getSaleByTitle', errorBoundary(getSalesByTitleController));
+router.post('/receiveCards', errorBoundary(receiveCardsValidationController));
+router.post('/receiveCards', errorBoundary(receiveCardsController));
+router.get('/suspendSale', errorBoundary(getSuspendedSalesController));
+router.get('/suspendSale/:id', errorBoundary(suspendedSaleByIdController));
+router.post('/suspendSale', errorBoundary(createSuspendedSaleController));
+router.delete('/suspendSale/:id', errorBoundary(deleteSuspendedSaleController));
+router.get('/getCardsByFilter', errorBoundary(getCardsByFilterController));
+router.get('/getDistinctSetNames', errorBoundary(distinctSetNamesController));
+router.get('/getCardsWithInfo', errorBoundary(getCardsWithInfoController));
+router.get('/getReceivedCards', errorBoundary(getReceivedCardsController));
+router.get(
+    '/getReceivedCards/:id',
+    errorBoundary(getReceivedCardsByIdController)
+);
+router.get('/getSalesReport', errorBoundary(salesReportController));
+router.get('/bulkSearch', errorBoundary(bulkSearchController));
 
 export default router;

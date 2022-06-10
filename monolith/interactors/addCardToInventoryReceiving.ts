02402024656ec8d03e741fdcdffdbe57a34d9540
Jai Kamat <jaipkamat@gmail.com>
2022-06-10 16:27:22 -0400

    chore: move addCardToInventoryReceiving to mongoose

--- monolith/interactors/addCardToInventoryReceiving.ts
+++ monolith/interactors/addCardToInventoryReceiving.ts
@@ -1,10 +1,9 @@
-import mongoose, { Collection } from 'mongoose';
-import { ClubhouseLocation, Trade } from '../common/types';
-import collectionFromLocation from '../lib/collectionFromLocation';
+import { ClubhouseLocation, FinishCondition, Trade } from '../common/types';
+import getInventoryCardModel from '../models/InventoryCardModel';
 
 export type ReceivingCard = {
     quantity: number;
-    finishCondition: string;
+    finishCondition: FinishCondition;
     id: string;
     name: string;
     set_name: string;
@@ -18,25 +17,24 @@ export type ReceivingCard = {
 // `finishCondition` Refers to the configuration of Finishes and Conditions ex. NONFOIL_NM or FOIL_LP
 async function addCardToInventoryReceiving(
     { quantity, finishCondition, id, name, set_name, set }: ReceivingCard,
-    // TODO: remove any and replace with mongoose schema queries
-    collection: any
+    location: ClubhouseLocation
 ) {
     try {
         console.log(
             `Receiving Info: QTY:${quantity}, ${finishCondition}, ${name}, ${id}`
         );
 
+        const InventoryCardModel = getInventoryCardModel(location);
+
         // Upsert the new quantity in the document
-        return await collection.updateOne(
+        return await InventoryCardModel.updateOne(
             { _id: id },
             {
-                $inc: {
-                    [`qoh.${finishCondition}`]: quantity,
-                },
+                $inc: { [`qoh.${finishCondition}`]: quantity },
                 $setOnInsert: { name, set_name, set },
             },
             { upsert: true }
-        );
+        ).exec();
     } catch (err) {
         console.log(err);
         throw err;
@@ -49,17 +47,11 @@ async function wrapAddCardToInventoryReceiving(
     location: ClubhouseLocation
 ) {
     try {
-        const db = mongoose.connection.db;
+        const operations = cards.map(async (receivingCard) => {
+            return addCardToInventoryReceiving(receivingCard, location);
+        });
 
-        const collection = db.collection(
-            collectionFromLocation(location).cardInventory
-        );
-
-        const promises = cards.map(async (c) =>
-            addCardToInventoryReceiving(c, collection)
-        );
-
-        const messages = await Promise.all(promises);
+        const messages = await Promise.all(operations);
 
         console.log(`Receiving cards at ${location}`);
 

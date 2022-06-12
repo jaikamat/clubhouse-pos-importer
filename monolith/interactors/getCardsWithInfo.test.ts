67f29c40764fbe159a0a8f7df9fa39f57ac38f23
Jai Kamat <jaipkamat@gmail.com>
2022-06-12 12:17:31 -0400

    refactor: migrate getCardsWithInfo to mongoose models

--- monolith/interactors/getCardsWithInfo.test.ts
+++ monolith/interactors/getCardsWithInfo.test.ts
@@ -1,14 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
 import Connection from '../database';
 import bulkCard from '../fixtures/fixtures';
+import ScryfallCardModel from '../models/ScryfallCardModel';
 import addCardToInventory from './addCardToInventory';
 import getCardsWithInfo from './getCardsWithInfo';
 
-const SCRYFALL_BULK = 'scryfall_bulk_cards';
-
 let mongoServer;
-let db;
 
 // Set up the mongo memory instance
 beforeAll(async () => {
@@ -16,13 +13,9 @@ beforeAll(async () => {
     const uri = mongoServer.getUri();
 
     await Connection.open(uri);
-    db = mongoose.connection.db;
-
-    // Create fake bulk collection
-    const bulkCollection = await db.createCollection(SCRYFALL_BULK);
 
     // Insert one bulk card
-    await bulkCollection.insert(bulkCard);
+    await ScryfallCardModel.collection.insertOne(bulkCard);
 
     // Add some cards to store inventory collection
     await addCardToInventory({
@@ -55,3 +48,32 @@ test('Fetching the bulk card to ensure it persisted', async () => {
         }
     `);
 });
+
+test('Fetching the bulk card without matching "in stock"', async () => {
+    // Remove the cards from inventory
+    await addCardToInventory({
+        quantity: -4,
+        finishCondition: 'FOIL_NM',
+        id: bulkCard.id,
+        name: bulkCard.name,
+        set_name: bulkCard.set_name,
+        set: bulkCard.set,
+        location: 'ch1',
+    });
+
+    const foundDoc = await getCardsWithInfo('Time Spiral', false, 'ch1');
+
+    expect(foundDoc.length).toBe(1);
+
+    expect(foundDoc[0].name).toMatchInlineSnapshot(`"Time Spiral"`);
+    expect(foundDoc[0].id).toMatchInlineSnapshot(
+        `"f3d62dbd-63db-4ac9-950f-9852627f23f2"`
+    );
+    expect(foundDoc[0].qoh).toMatchInlineSnapshot(`
+        Object {
+          "FOIL_NM": 0,
+        }
+    `);
+});
+
+// TODO: do a test with `matchInStock` boolean
--- monolith/interactors/getCardsWithInfo.ts
+++ monolith/interactors/getCardsWithInfo.ts
@@ -1,11 +1,23 @@
-import mongoose from 'mongoose';
-import RawScryfallCard from '../common/RawScryfallCard';
 import { ScryfallApiCard } from '../common/ScryfallApiCard';
-import { ClubhouseLocation, Collection, QOH } from '../common/types';
-import collectionFromLocation from '../lib/collectionFromLocation';
+import { ClubhouseLocation, QOH } from '../common/types';
+import getInventoryCardModel from '../models/InventoryCardModel';
+import ScryfallCardModel from '../models/ScryfallCardModel';
+import { ScryfallCard } from '../schemas/ScryfallCardSchema';
 
-type AggregationCard = RawScryfallCard & { qoh: QOH };
 type ReturnCard = ScryfallApiCard & { qoh: QOH };
+type AggregateCard = ScryfallCard & { qoh?: QOH };
+
+/**
+ * Transform the bulk cards and reattach existing QOH.
+ *
+ * QOH may not exist yet if a card is not in inventory, so we need to add it.
+ */
+const transformAggregateCard = (card: AggregateCard): ReturnCard => {
+    return {
+        ...new ScryfallApiCard(card),
+        qoh: card.qoh ? card.qoh : {},
+    };
+};
 
 async function getCardsWithInfo(
     title: string,
@@ -14,13 +26,8 @@ async function getCardsWithInfo(
     location: ClubhouseLocation
 ): Promise<ReturnCard[]> {
     try {
-        const db = await mongoose.connection.db;
-
-        const pipeline = [];
-
-        // Fetch by title
-        const match = {
-            $match: {
+        const results = await ScryfallCardModel.aggregate<AggregateCard>()
+            .match({
                 name: title,
                 $or: [
                     /**
@@ -29,77 +36,48 @@ async function getCardsWithInfo(
                      * ex. World Championship cards, and freshly-added cards are in this list
                      */
                     {
-                        'games.0': {
-                            $exists: false,
-                        },
+                        'games.0': { $exists: false },
                     },
                     /**
                      * We also want to include cards that, if they _do_ have a `games` array,
                      * include "paper" as a game type
                      */
                     {
-                        games: {
-                            $in: ['paper'],
-                        },
+                        games: { $in: ['paper'] },
                     },
                 ],
-            },
-        };
-
-        // Zip up bulk with qoh
-        const lookup = {
-            $lookup: {
-                from: collectionFromLocation(location).cardInventory,
+            })
+            .lookup({
+                from: getInventoryCardModel(location).collection.name,
                 localField: 'id',
                 foreignField: '_id',
                 as: 'qoh',
-            },
-        };
-
-        // $lookup yields an array - replace the new qoh with the nested card value
-        const addFields = {
-            $addFields: {
+            })
+            .addFields({
                 qoh: { $arrayElemAt: ['$qoh.qoh', 0] },
-            },
-        };
+            })
+            .match(
+                matchInStock
+                    ? {
+                          $or: [
+                              { 'qoh.FOIL_NM': { $gt: 0 } },
+                              { 'qoh.FOIL_LP': { $gt: 0 } },
+                              { 'qoh.FOIL_MP': { $gt: 0 } },
+                              { 'qoh.FOIL_HP': { $gt: 0 } },
+                              { 'qoh.NONFOIL_NM': { $gt: 0 } },
+                              { 'qoh.NONFOIL_LP': { $gt: 0 } },
+                              { 'qoh.NONFOIL_MP': { $gt: 0 } },
+                              { 'qoh.NONFOIL_HP': { $gt: 0 } },
+                              { 'qoh.ETCHED_NM': { $gt: 0 } },
+                              { 'qoh.ETCHED_LP': { $gt: 0 } },
+                              { 'qoh.ETCHED_MP': { $gt: 0 } },
+                              { 'qoh.ETCHED_HP': { $gt: 0 } },
+                          ],
+                      }
+                    : {}
+            );
 
-        // Only yield cards that are in-stock, if needed
-        const inventoryMatch = {
-            $match: {
-                $or: [
-                    { 'qoh.FOIL_NM': { $gt: 0 } },
-                    { 'qoh.FOIL_LP': { $gt: 0 } },
-                    { 'qoh.FOIL_MP': { $gt: 0 } },
-                    { 'qoh.FOIL_HP': { $gt: 0 } },
-                    { 'qoh.NONFOIL_NM': { $gt: 0 } },
-                    { 'qoh.NONFOIL_LP': { $gt: 0 } },
-                    { 'qoh.NONFOIL_MP': { $gt: 0 } },
-                    { 'qoh.NONFOIL_HP': { $gt: 0 } },
-                    { 'qoh.ETCHED_NM': { $gt: 0 } },
-                    { 'qoh.ETCHED_LP': { $gt: 0 } },
-                    { 'qoh.ETCHED_MP': { $gt: 0 } },
-                    { 'qoh.ETCHED_HP': { $gt: 0 } },
-                ],
-            },
-        };
-
-        pipeline.push(match);
-        pipeline.push(lookup);
-        pipeline.push(addFields);
-        if (matchInStock) pipeline.push(inventoryMatch);
-
-        // TODO: remove any and replace with mongoose schema queries
-        const cards: any = await db
-            .collection(Collection.scryfallBulkCards)
-            .aggregate(pipeline)
-            .toArray();
-
-        // Transform the bulk cards and reattach existing QOH
-        return cards.map((c) => ({
-            ...new ScryfallApiCard(c),
-            // QOH may not exist yet if a card is not in inventory, so we need to add it
-            qoh: c.qoh ? c.qoh : {},
-        }));
+        return results.map(transformAggregateCard);
     } catch (err) {
         console.log(err);
         throw err;

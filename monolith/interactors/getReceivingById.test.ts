5d563acecd7fe0dfd7eb410619e9b49183318535
Jai Kamat <jaipkamat@gmail.com>
2022-07-03 16:54:18 -0400

    chore: refactor getReceivingById to use mongoose

---
+++ monolith/interactors/getReceivingById.test.ts
@@ -0,0 +1,53 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { Collection } from '../common/types';
+import Connection from '../database';
+import bulkCard from '../fixtures/fixtures';
+import ScryfallCardModel from '../models/ScryfallCardModel';
+import getReceivingById from './getReceivingById';
+
+let mongoServer;
+
+// Set up the mongo memory instance
+beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+
+    await Connection.open(uri);
+
+    // Insert one bulk card
+    await ScryfallCardModel.collection.insertOne(bulkCard);
+
+    // Insert user
+    // TODO: Replace with mongoose schema queries
+    const db = await mongoose.connection.db;
+    await db.collection(Collection.users).insertOne({
+        username: 'Jane Doe',
+        password: 'password',
+        locations: ['ch1'],
+        lightspeedEmployeeNumber: 17,
+    });
+
+    // Create receiving entity
+    await db.collection(Collection.receivedCards).insertOne({
+        _id: new mongoose.Types.ObjectId('receiving-id'),
+        created_at: 'date',
+        employee_number: 17,
+        received_card_list: [{ id: bulkCard.id }],
+        created_by: new mongoose.Types.ObjectId('user-id-3456'),
+        customer_contact: null,
+        customer_name: 'Jane Doe',
+    });
+});
+
+afterAll(async () => {
+    await mongoServer.stop();
+});
+
+test.only('Fetch the receiving entity to ensure it persisted', async () => {
+    const foundDoc: any = await getReceivingById('receiving-id', 'ch1');
+
+    expect(foundDoc.customer_name).toBe('Jane Doe');
+    expect(foundDoc.received_cards.length).toBe(1);
+    expect(foundDoc.received_cards[0].bulk_card_data.id).toBe(bulkCard.id);
+});
--- monolith/interactors/getReceivingById.ts
+++ monolith/interactors/getReceivingById.ts
@@ -2,100 +2,88 @@ import mongoose from 'mongoose';
 import { ScryfallApiCard } from '../common/ScryfallApiCard';
 import { ClubhouseLocation, Collection } from '../common/types';
 import collectionFromLocation from '../lib/collectionFromLocation';
+import getReceivedCardsModel from '../models/ReceivedCardsModel';
+import { ScryfallCard } from '../schemas/ScryfallCardSchema';
+
+interface AggregateResult {
+    _id: string;
+    created_at: string;
+    customer_name: string;
+    customer_contact?: string;
+    received_cards: Array<{ bulk_card_data: ScryfallCard }>;
+}
 
 async function getReceivingById(id: string, location: ClubhouseLocation) {
     try {
-        const db = await mongoose.connection.db;
-        const collection = collectionFromLocation(location).receivedCards;
-
-        let aggregation = [];
-
-        const match = { _id: new mongoose.Types.ObjectId(id) };
-
-        const addFields = {
-            created_at: {
-                $toDate: '$_id',
-            },
-            /** Convert to ObjectId for proper $lookup types */
-            user_id: {
-                $toObjectId: '$created_by',
-            },
-        };
-
-        /**
-         * We zip up bulk card data here and alias it
-         * to `scryfall_cards` for downstream use.
-         */
-        const bulkLookup = {
-            from: Collection.scryfallBulkCards,
-            localField: 'received_card_list.id',
-            foreignField: 'id',
-            as: 'scryfall_cards',
-        };
-
-        /**
-         * Join users into the aggregation and replace created_by with user entity
-         */
-        const userLookup = {
-            from: collectionFromLocation(location).users,
-            localField: 'user_id',
-            foreignField: '_id',
-            as: 'created_by',
-        };
-
-        const project = {
-            created_at: 1,
-            created_by: {
-                $first: '$created_by',
-            },
-            customer_name: 1,
-            customer_contact: 1,
-            /**
-             * We iterate over the previous card list and replace
-             * its entries with scryfall bulk data through `$filter`
-             * and `id` equivalence.
-             *
-             * `$filter` resolves to an array, hence wrapping in `$arrayElemAt`.
-             */
-            received_cards: {
-                $map: {
-                    input: '$received_card_list',
-                    as: 'item',
-                    in: {
-                        quantity: '$$item.quantity',
-                        marketPrice: '$$item.marketPrice',
-                        cashPrice: '$$item.cashPrice',
-                        creditPrice: '$$item.creditPrice',
-                        tradeType: '$$item.tradeType',
-                        finishCondition: '$$item.finishCondition',
-                        bulk_card_data: {
-                            $first: {
-                                $filter: {
-                                    input: '$scryfall_cards',
-                                    as: 'card',
-                                    cond: {
-                                        $eq: ['$$item.id', '$$card.id'],
+        const results = await getReceivedCardsModel(location)
+            .aggregate<AggregateResult>()
+            .match({ _id: new mongoose.Types.ObjectId(id) })
+            .addFields({
+                created_at: {
+                    $toDate: '$_id',
+                },
+                /** Convert to ObjectId for proper $lookup types */
+                user_id: {
+                    $toObjectId: '$created_by',
+                },
+            })
+            .lookup({
+                from: Collection.scryfallBulkCards,
+                localField: 'received_card_list.id',
+                foreignField: 'id',
+                as: 'scryfall_cards',
+            })
+            .lookup({
+                from: collectionFromLocation(location).users,
+                localField: 'user_id',
+                foreignField: '_id',
+                as: 'created_by',
+            })
+            .project({
+                created_at: 1,
+                created_by: {
+                    $arrayElemAt: ['$created_by', 0],
+                },
+                customer_name: 1,
+                customer_contact: 1,
+                /**
+                 * We iterate over the previous card list and replace
+                 * its entries with scryfall bulk data through `$filter`
+                 * and `id` equivalence.
+                 *
+                 * `$filter` resolves to an array, hence wrapping in `$arrayElemAt`.
+                 */
+                received_cards: {
+                    $map: {
+                        input: '$received_card_list',
+                        as: 'item',
+                        in: {
+                            quantity: '$$item.quantity',
+                            marketPrice: '$$item.marketPrice',
+                            cashPrice: '$$item.cashPrice',
+                            creditPrice: '$$item.creditPrice',
+                            tradeType: '$$item.tradeType',
+                            finishCondition: '$$item.finishCondition',
+                            bulk_card_data: {
+                                $arrayElemAt: [
+                                    {
+                                        $filter: {
+                                            input: '$scryfall_cards',
+                                            as: 'card',
+                                            cond: {
+                                                $eq: ['$$item.id', '$$card.id'],
+                                            },
+                                        },
                                     },
-                                },
+                                    0,
+                                ],
                             },
                         },
                     },
                 },
-            },
-        };
+            });
 
-        aggregation.push({ $match: match });
-        aggregation.push({ $addFields: addFields });
-        aggregation.push({ $lookup: bulkLookup });
-        aggregation.push({ $lookup: userLookup });
-        aggregation.push({ $project: project });
-
-        const doc = await db
-            .collection(collection)
-            .aggregate(aggregation)
-            .toArray();
-
-        const received = doc[0];
+        const received = results[0];
 
         return {
             ...received,
